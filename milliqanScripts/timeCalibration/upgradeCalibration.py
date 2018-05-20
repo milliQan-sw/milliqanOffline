d8582727e5c8bd75727adeedc39c6293cb3518b5
Ryan Heller <heller@physics.ucsb.edu>
2018-05-20 12:05:25 -0700

    updated timing calib to include panel/slab and fix ch8

--- milliqanScripts/timeCalibration/upgradeCalibration.py
+++ milliqanScripts/timeCalibration/upgradeCalibration.py
@@ -5,12 +5,15 @@ import copy
 r.gROOT.SetBatch(True)
 r.gStyle.SetOptFit(11111)
 
-fNameTag = '816'
+beamOn = False
+fNameTag = '842'
 # fNameTag = '423'
 cosmicHeightSelections = {}
 b2Delta = 16
+topPanels = [10,14,30]
 for i in range(32):
     cosmicHeightSelections[i] = 500
+#878 + 7725s 
 cosmicHeightSelections[8+b2Delta] = 1150
 cosmicHeightSelections[9+b2Delta] = 1150
 cosmicHeightSelections[8] = 1150
@@ -18,9 +21,27 @@ cosmicHeightSelections[9] = 1150
 cosmicHeightSelections[1+b2Delta] = 1150
 cosmicHeightSelections[5] = 1150
 cosmicHeightSelections[6+b2Delta] = 1150
-inputFile = r.TFile("~/Run816.root")
+#Panels
+#Top
+cosmicHeightSelections[10] = 20
+cosmicHeightSelections[14] = 20
+cosmicHeightSelections[14+b2Delta] = 20
+#Left
+cosmicHeightSelections[11] = 20
+cosmicHeightSelections[11+b2Delta] = 20
+cosmicHeightSelections[15+b2Delta] = 20
+#Right
+cosmicHeightSelections[13+b2Delta] = 20
+cosmicHeightSelections[3+b2Delta] = 20
+cosmicHeightSelections[10+b2Delta] = 20
+#Slabs
+cosmicHeightSelections[2+b2Delta] = 200
+cosmicHeightSelections[4+b2Delta] = 200
+cosmicHeightSelections[5+b2Delta] = 200
+cosmicHeightSelections[12+b2Delta] = 200
+inputFile = r.TFile("/net/cms26/cms26r0/milliqan/milliqanOffline/testCalibration.root")
 # inputFile = r.TFile("./Run423.root")
-def upgradeCalibrationIntraLayerPlots(allSlices):
+def upgradeCalibrationIntraLayerPlots(allSlices,sliceExtraSelections,ignoreChans):
     inputTree = inputFile.Get("t")
     #800
     # cosmicHeightSelections = {0:50,1:50,2:5,3:5,4:5,5:5,6:50,7:50,8:50,9:50,10:50,11:50,12:50,13:5,14:50,15:50}
@@ -32,22 +53,26 @@ def upgradeCalibrationIntraLayerPlots(allSlices):
         for sliceNum,ySlice in slices.iteritems():
             name="_".join([str(x) for x in ySlice])
             for higherChan in ySlice[1:]:
-                calibHistos[sName,higherChan,ySlice[0]] = r.TH1D("chans_{0}_{1}".format(higherChan,ySlice[0],name),"",96,-30,30)
+                calibHistos[sName,higherChan,ySlice[0]] = r.TH1D("chans_{0}_{1}".format(higherChan,ySlice[0],name),"",160,-50,50)
                 calibHistos[sName,higherChan,ySlice[0]].SetDirectory(0)
     for event in inputTree:
         if iE % int(nEvents/5) == 0: 
             print iE*1./nEvents
+        if event.beam != beamOn:
+            iE += 1
+            continue
         chans = event.chan
         heights = event.height
         times = event.time
         firstPulses = {}
         for chan,height,time in zip(chans,heights,times):
+            if chan in ignoreChans: continue
             if chan not in firstPulses and height > cosmicHeightSelections[chan]:
                 firstPulses[chan] = time
         for sName,slices in allSlices.iteritems():
             for sliceNum,ySlice in slices.iteritems():
-                if len(firstPulses) > len(ySlice):continue
-                if set(ySlice).issubset(firstPulses.keys()):
+                if len(firstPulses) > len(ySlice+sliceExtraSelections[sliceNum]):continue
+                if set(ySlice+sliceExtraSelections[sliceNum]).issubset(firstPulses.keys()):
                     for higherChan in ySlice[1:]:
                         calibHistos[sName,higherChan,ySlice[0]].Fill(firstPulses[higherChan]-firstPulses[ySlice[0]])
         iE += 1
@@ -68,8 +93,8 @@ def upgradeCalibrationIntraLayerCalculateCalibration(inFile,allSlices):
     sliceHists = {}
     for sliceNum,ySlice in allSlices["slicesStraight"].iteritems():
         if sliceNum % 2 == 0:
-            sliceHists[sliceNum,sliceNum-1] = r.TH1D("chans_{0}_{1}".format(sliceNum,sliceNum-1),"",96,-30,30)
-        sliceHists[sliceNum,sliceNum] = r.TH1D("chans_{0}_{1}".format(sliceNum,sliceNum),"",96,-30,30)
+            sliceHists[sliceNum,sliceNum-1] = r.TH1D("chans_{0}_{1}".format(sliceNum,sliceNum-1),"",160,-50,50)
+        sliceHists[sliceNum,sliceNum] = r.TH1D("chans_{0}_{1}".format(sliceNum,sliceNum),"",160,-50,50)
         #For straight path hists make calib to correct high channels down to bottom
         name="_".join([str(x) for x in ySlice])
         chanToSliceDict[ySlice[0]] = sliceNum
@@ -106,40 +131,104 @@ def upgradeCalibrationIntraLayerCalculateCalibration(inFile,allSlices):
     for (slice1,slice2),hist in sliceHists.iteritems():
         if slice2 == slice1-1:
             for chan in allSlices["slicesStraight"][slice1]:
+                if chan in topPanels: continue
                 calibConstantsIntraLayer[chan] -= (hist.GetMean()/0.625)*0.625
     outFile = r.TFile('calibInterSlice'+fNameTag+'.root','RECREATE')
     outFile.cd()
     for _,sliceHist in sliceHists.iteritems():
         sliceHist.Write()
     return calibConstantsIntraLayer
-def upgradeCalibrationInterLayer(calibConstantsIntraLayer,allLayerPaths):
+def upgradeCalibrationSlabPanel(calibConstantsInterLayer,slabCalibrations,slabExtraSelections,panelExtraSelections):
+    inputTree = inputFile.Get("t")
+    calibHistosSlab = {}
+    for slabNum,layers in slabCalibrations.iteritems():
+        calibHistosSlab[slabNum] = r.TH1D("chans_{0}".format(slabNum),"",160,-50,50)
+        calibHistosSlab[slabNum].SetDirectory(0)
+    calibHistosPanel = {}
+    for panelNum,layers in panelCalibrations.iteritems():
+        calibHistosPanel[panelNum] = r.TH1D("chans_{0}".format(panelNum),"",160,-50,50)
+        calibHistosPanel[panelNum].SetDirectory(0)
+    iE = 1
+    nEvents = inputTree.GetEntries()
+    for event in inputTree:
+        if iE % int(nEvents/5) == 0: 
+            print iE*1./nEvents
+        if event.beam != beamOn:
+            iE += 1
+            continue
+        chans = event.chan
+        heights = event.height
+        times = event.time
+        layers = event.layer
+        firstPulses = {}
+        firstPulsesExtra = {}
+        for layer,height,time,chan in zip(layers,heights,times,chans):
+            if chan not in firstPulsesExtra and height > cosmicHeightSelections[chan]:
+                firstPulsesExtra[chan] = time
+            if chan not in calibConstantsInterLayer: continue
+            if layer not in firstPulses and height > cosmicHeightSelections[chan]:
+                firstPulses[layer] = time+calibConstantsInterLayer[chan]
+        if set(slabExtraSelections).issubset(set(firstPulsesExtra.keys())):
+            for slabNum,layers in slabCalibrations.iteritems():
+                if set(layers).issubset(firstPulses):
+                    for layer in layers:
+                        if slabNum in firstPulsesExtra:
+                            calibHistosSlab[slabNum].Fill(firstPulsesExtra[slabNum]-firstPulses[layer])
+        for panelNum,layers in panelCalibrations.iteritems():
+            if set(panelExtraSelections[panelNum]).issubset(set(firstPulsesExtra.keys())):
+                if set(layers).issubset(firstPulses): 
+                    for layer in layers:
+                        if panelNum in firstPulsesExtra:
+                            calibHistosPanel[panelNum].Fill(firstPulsesExtra[panelNum]-firstPulses[layer])
+        iE += 1
+    outputFile = r.TFile("calibSlabPanel"+fNameTag+".root","RECREATE")
+    calibMeans = {}
+    calibConstantsInterLayerSlabPanel = copy.deepcopy(calibConstantsInterLayer)
+    for slabNum,layers in slabCalibrations.iteritems():
+        calibHistosSlab[slabNum].Write()
+        calibMeans[slabNum] = (calibHistosSlab[slabNum].GetMean()/0.625)*0.625
+        calibConstantsInterLayerSlabPanel[slabNum] = -calibMeans[slabNum]
+    for panelNum,layers in panelCalibrations.iteritems():
+        calibHistosPanel[panelNum].Write()
+        calibMeans[panelNum] = (calibHistosPanel[panelNum].GetMean()/0.625)*0.625
+        calibConstantsInterLayerSlabPanel[panelNum] = -calibMeans[panelNum]
+
+    return calibConstantsInterLayerSlabPanel
+def upgradeCalibrationInterLayer(calibConstantsIntraLayer,allLayerPaths,layerExtraSelections):
     calibConstantsFinal = {}
     inputTree = inputFile.Get("t")
-    layerToChannel = {1:(0,1,24,25,8,9),2:(6,7,16,17,12,13),3:(2,3,22,23,4,5)}
+    layerToChannel = {1:(0,1,24,25,9),2:(6,7,16,17,12,13),3:(2,3,22,23,4,5)}
     calibHistos = {}
     for pathNum,layers in allLayerPaths.iteritems():
         for higherLayer in layers[1:]:
-            calibHistos[higherLayer,layers[0]] = r.TH1D("chans_{0}_{1}".format(higherLayer,layers[0]),"",96,-30,30)
+            calibHistos[higherLayer,layers[0]] = r.TH1D("chans_{0}_{1}".format(higherLayer,layers[0]),"",160,-50,50)
             calibHistos[higherLayer,layers[0]].SetDirectory(0)
     iE = 1
     nEvents = inputTree.GetEntries()
     for event in inputTree:
         if iE % int(nEvents/5) == 0: 
             print iE*1./nEvents
+        if event.beam != beamOn:
+            iE += 1
+            continue
         chans = event.chan
         heights = event.height
         times = event.time
         layers = event.layer
         firstPulses = {}
+        firstPulsesExtra = {}
         for layer,height,time,chan in zip(layers,heights,times,chans):
+            if chan not in firstPulsesExtra and height > cosmicHeightSelections[chan]:
+                firstPulsesExtra[chan] = time
             if chan not in calibConstantsIntraLayer: continue
             if layer not in firstPulses and height > cosmicHeightSelections[chan]:
                 firstPulses[layer] = time+calibConstantsIntraLayer[chan]
-        for pathNum,layers in allLayerPaths.iteritems():
-            # if len(firstPulses) > len(layers):continue
-            if set(layers).issubset(firstPulses.keys()):
-                for higherLayer in layers[1:]:
-                    calibHistos[higherLayer,layers[0]].Fill(firstPulses[higherLayer]-firstPulses[layers[0]])
+        if set(layerExtraSelections).issubset(set(firstPulsesExtra.keys())):
+            for pathNum,layers in allLayerPaths.iteritems():
+                # if len(firstPulses) > len(layers):continue
+                if set(layers).issubset(firstPulses.keys()):
+                    for higherLayer in layers[1:]:
+                        calibHistos[higherLayer,layers[0]].Fill(firstPulses[higherLayer]-firstPulses[layers[0]])
         iE += 1
     outputFile = r.TFile("calibInterLayer"+fNameTag+".root","RECREATE")
     calibConstantsInterLayer = copy.deepcopy(calibConstantsIntraLayer)
@@ -160,22 +249,46 @@ def upgradeCalibrationInterLayer(calibConstantsIntraLayer,allLayerPaths):
     return calibConstantsInterLayer
 if __name__=="__main__":
     allSlices = {}
-    # allSlices["slicesStraight"] = {1:(0,2,4),2:(1,3,5),3:(6,12,8),4:(7,13,9),5:(14,10),6:(15,11)}
-    # allSlices["slicesBent1"] = {1:(1,2,4),2:(0,3,5),3:(7,12,8),4:(6,13,9),5:(15,10),6:(14,11)}
-    # allSlices["slicesBent2"] = {1:(1,3,4),2:(0,2,5),3:(7,13,8),4:(6,12,9)}
-    # allSlices["slicesBent3"] = {1:(1,2,3,4),2:(0,2,3,5),3:(7,12,13,8),4:(6,12,13,9)}
-    allSlices["slicesStraight"] = {1:(0,8+b2Delta,8),2:(1,9+b2Delta,9),3:(6,0+b2Delta,12),4:(7,1+b2Delta,13),5:(2,6+b2Delta,4),6:(3,7+b2Delta,5)}
-    allSlices["slicesBent1"] = {1:(1,8+b2Delta,8),2:(0,9+b2Delta,9),3:(7,0+b2Delta,12),4:(6,1+b2Delta,13),5:(3,6+b2Delta,4),6:(2,7+b2Delta,5)}
-    allSlices["slicesBent2"] = {1:(1,9+b2Delta,8),2:(0,8+b2Delta,9),3:(7,1+b2Delta,12),4:(6,0+b2Delta,13),5:(3,7+b2Delta,4),6:(2,6+b2Delta,5)}
-    allSlices["slicesBent3"] = {1:(1,8+b2Delta,9+b2Delta,8),2:(0,8+b2Delta,9+b2Delta,9),3:(7,0+b2Delta,1+b2Delta,12),4:(6,0+b2Delta,1+b2Delta,13),5:(3,6+b2Delta,7+b2Delta,4),6:(2,6+b2Delta,7+b2Delta,5)}
+    #orig
+    # allSlices["slicesStraight"] = {1:(0,8+b2Delta,8),2:(1,9+b2Delta,9),3:(6,0+b2Delta,12),4:(7,1+b2Delta,13),5:(2,6+b2Delta,4),6:(3,7+b2Delta,5)}
+    # allSlices["slicesBent1"] = {1:(1,8+b2Delta,8),2:(0,9+b2Delta,9),3:(7,0+b2Delta,12),4:(6,1+b2Delta,13),5:(3,6+b2Delta,4),6:(2,7+b2Delta,5)}
+    # allSlices["slicesBent2"] = {1:(1,9+b2Delta,8),2:(0,8+b2Delta,9),3:(7,1+b2Delta,12),4:(6,0+b2Delta,13),5:(3,7+b2Delta,4),6:(2,6+b2Delta,5)}
+    # allSlices["slicesBent3"] = {1:(1,8+b2Delta,9+b2Delta,8),2:(0,8+b2Delta,9+b2Delta,9),3:(7,0+b2Delta,1+b2Delta,12),4:(6,0+b2Delta,1+b2Delta,13),5:(3,6+b2Delta,7+b2Delta,4),6:(2,6+b2Delta,7+b2Delta,5)}
+    #using 10
+    layerExtraSelections=[]#2+b2Delta]#,4+b2Delta,12+b2Delta,5+b2Delta]
+    allSlabs=[2+b2Delta,4+b2Delta,12+b2Delta,5+b2Delta]
+    allPanels=[11+b2Delta,11,15+b2Delta,13+b2Delta,3+b2Delta,10+b2Delta,10,14+b2Delta,14]
+    slabExtraSelections = {}
+    for slab in allSlabs:
+        slabExtraSelections[slab] = []
+    panelExtraSelections = {}
+    for panel in allPanels:
+        panelExtraSelections[panel] = []
+    # layerExtraSelections=[]
+    ignoreChansForSlices = [2+b2Delta,4+b2Delta,12+b2Delta,5+b2Delta,11+b2Delta,11,15+b2Delta,13+b2Delta,3+b2Delta,10+b2Delta]
+    sliceExtraSelections={1:[10],2:[10],3:[14+b2Delta],4:[14+b2Delta],5:[14],6:[14]}
+    allSlices["slicesStraight"] = {1:[0,8+b2Delta],2:[1,9+b2Delta,9],3:[6,0+b2Delta,12],4:[7,1+b2Delta,13],5:[2,6+b2Delta,4],6:[3,7+b2Delta,5]}
+    allSlices["slicesBent1"] = {1:[1,8+b2Delta],2:[0,9+b2Delta,9],3:[7,0+b2Delta,12],4:[6,1+b2Delta,13],5:[3,6+b2Delta,4],6:[2,7+b2Delta,5]}
+    allSlices["slicesBent2"] = {1:[1,9+b2Delta],2:[0,8+b2Delta,9],3:[7,1+b2Delta,12],4:[6,0+b2Delta,13],5:[3,7+b2Delta,4],6:[2,6+b2Delta,5]}
+    allSlices["slicesBent3"] = {1:[1,8+b2Delta,9+b2Delta],2:[0,8+b2Delta,9+b2Delta,9],3:[7,0+b2Delta,1+b2Delta,12],4:[6,0+b2Delta,1+b2Delta,13],5:[3,6+b2Delta,7+b2Delta,4],6:[2,6+b2Delta,7+b2Delta,5]}
     # allSlices["slicesBent3"] = {1:(1,2,3,4),2:(0,2,3,5),3:(7,12,13,8),4:(6,12,13,9)}
     allLayerPaths = {1:(1,2),2:(2,3)}
-    upgradeCalibrationIntraLayerPlots(allSlices)
+
+    slabCalibrations={2+b2Delta:[1,2],4+b2Delta:[1,2],12+b2Delta:[2,3],5+b2Delta:[2,3]}
+    panelCalibrations={11+b2Delta:[1],11:[2],15+b2Delta:[3],13+b2Delta:[1],3+b2Delta:[2],10+b2Delta:[3],10:[1],14+b2Delta:[2],14:[3]}
+    slabExtraSelections=[]
+    upgradeCalibrationIntraLayerPlots(allSlices,sliceExtraSelections,ignoreChansForSlices)
     print "Done intra slice"
     calibConstantsIntraLayer = upgradeCalibrationIntraLayerCalculateCalibration("calibIntraSlice"+fNameTag+".root",allSlices)
     print "Done inter slice"
-    calibConstantsInterLayer = upgradeCalibrationInterLayer(calibConstantsIntraLayer,allLayerPaths)
+    pickle.dump(calibConstantsIntraLayer,open("calibConstantsIntraLayer"+fNameTag+".pkl",'w'))
+    calibConstantsInterLayer = upgradeCalibrationInterLayer(calibConstantsIntraLayer,allLayerPaths,layerExtraSelections)
     print "Done inter layer"
+    pickle.dump(calibConstantsInterLayer,open("calibConstantsInterLayer"+fNameTag+".pkl",'w'))
+    calibConstantsInterLayerSlabPanel = upgradeCalibrationSlabPanel(calibConstantsInterLayer,slabCalibrations,slabExtraSelections,panelExtraSelections)
+    pickle.dump(calibConstantsInterLayerSlabPanel,open("calibConstantsInterLayerSlabPanel"+fNameTag+".pkl",'w'))
+    print "Done panel/slabs"
 
     print calibConstantsIntraLayer
     print calibConstantsInterLayer
+    print calibConstantsInterLayerSlabPanel

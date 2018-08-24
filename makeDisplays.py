3f6253c5f444b31ca724145d4e3f03b56e9cef26
milliQan <heller@physics.ucsb.edu>
2018-08-23 17:03:17 -0700

    updated display code for filtering

--- makeDisplays.py
+++ makeDisplays.py
@@ -21,12 +21,13 @@ def parse_args():
 	parser.add_argument("--noBounds",help="Disable display of pulsefinding bounds.",action='store_true',default=False)
 	parser.add_argument("-c","--forceChans",nargs='+',help="List of channels to force in display (space separated, any length)")
 	parser.add_argument("-f","--fft",help="run FFT",action="store_true")
+	parser.add_argument("-l","--LPF",help="apply low pass filter",action="store_true")
 	parser.add_argument("-o","--onlyForceChans",action='store_true',help="Only show forced chans")
 	args = parser.parse_args()
 	#print args
 	return args
 
-def main(runNumber, selection, nEvents,tag="",rangeForTime=None,rangeForVoltage=None,noBounds=False,forceChans=[],onlyForceChans=False,fft=False):
+def main(runNumber, selection, nEvents,tag="",rangeForTime=None,rangeForVoltage=None,noBounds=False,forceChans=[],onlyForceChans=False,fft=False,LPF=False):
         if forceChans == None:
             forceChans = []
 	displayPulseBounds = not noBounds
@@ -46,7 +47,7 @@ def main(runNumber, selection, nEvents,tag="",rangeForTime=None,rangeForVoltage=
                     rangeForTime = ["-1","-1"]
                 if rangeForVoltage == None:
                     rangeForVoltage = ["-1000","-1000"]
-		args = ["make_tree",treeName,eventNumber,tag,str(rangeForTime[0]),str(rangeForTime[1]),str(rangeForVoltage[0]),str(rangeForVoltage[1]),str(int(displayPulseBounds)),str(int(onlyForceChans)),str(int(fft))]
+		args = ["./make_tree_LPFilter",treeName,eventNumber,tag,str(rangeForTime[0]),str(rangeForTime[1]),str(rangeForVoltage[0]),str(rangeForVoltage[1]),str(int(displayPulseBounds)),str(int(onlyForceChans)),str(int(fft)),str(int(LPF))]
 		if forceChans != None:
 			args = args + forceChans
 		# print args

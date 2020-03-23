cc335e2144f02a3a533bfcc2a60cc75995380788
Matthew Citron <mcitron@cern.ch>
2020-03-23 14:11:06 -0700

    rebin for low stats

--- milliqanScripts/timeCorrections.py
+++ milliqanScripts/timeCorrections.py
@@ -3,8 +3,10 @@ import bisect
 import pickle 
 r.gROOT.SetBatch()
 
-minBinsMC = {"878":3,"ET":2,"7725":4}
-minBinsData = {"878":6,"ET":2,"7725":4}
+minBinsMC = {"878":2,"ET":1,"7725":3}
+minBinsData = {"878":3,"ET":1,"7725":4}
+maxBinsMC = {"878":17,"ET":17,"7725":17}
+maxBinsData = {"878":18,"ET":16,"7725":16}
 def readTimeInputsPerSpecies(inputFileData,inputFileMC):
     #return time corrections from file (NB: should added to recorded time!)
     speciesList = ["878","ET","7725"]
@@ -21,22 +23,32 @@ def readTimeInputsPerSpecies(inputFileData,inputFileMC):
             areaBins.append(profHist.GetBinLowEdge(iBin))
             projData = inputFileData.Get("AreaProj{0}_{1}".format(species,iBin))
             projMC = inputFileMC.Get("AreaProj{0}_{1}".format(species,iBin))
-            maxBinData = projData.GetMaximumBin()
-            maxBinMC = projMC.GetMaximumBin()
+            if projData.GetBinContent(projData.GetMaximumBin()) < 20:
+                projData.Rebin(2)
+            if projData.GetBinContent(projData.GetMaximumBin()) < 20:
+                projData.Rebin(2)
+            # projData.Rebin(2)
+            binWithMaxData = projData.GetMaximumBin()
+            binWithMaxMC = projMC.GetMaximumBin()
             meanData,meanMC = 0,0
             weightData,weightMC = 0,0
-            for iRange in range(-2,3):
-                meanData += projData.GetBinCenter(maxBinData+iRange)*projData.GetBinContent(maxBinData+iRange)
-                weightData += projData.GetBinContent(maxBinData+iRange)
-                meanMC += projMC.GetBinCenter(maxBinMC+iRange)*projMC.GetBinContent(maxBinMC+iRange)
-                weightMC += projMC.GetBinContent(maxBinMC+iRange)
+            for iRange in range(-4,5):
+                meanData += projData.GetBinCenter(binWithMaxData+iRange)*projData.GetBinContent(binWithMaxData+iRange)
+                weightData += projData.GetBinContent(binWithMaxData+iRange)
+                meanMC += projMC.GetBinCenter(binWithMaxMC+iRange)*projMC.GetBinContent(binWithMaxMC+iRange)
+                weightMC += projMC.GetBinContent(binWithMaxMC+iRange)
             if weightData > 0:
                 meanData /= weightData
             if weightMC > 0: 
                 meanMC /= weightMC
-
-            correctionsData.append(-meanData)
-            correctionsMC.append(-meanMC)
+            if iBin > maxBinsData[species]:
+                correctionsData.append(correctionsData[-1])
+            else:
+                correctionsData.append(-meanData)
+            if iBin > maxBinsMC[species]:
+                correctionsMC.append(correctionsMC[-1])
+            else:
+                correctionsMC.append(-meanMC)
         for iBin in range(1,nBins+1):
             if iBin < minBinsMC[species]:
                 correctionsMC[iBin-1] = correctionsMC[minBinsMC[species]-1]
@@ -44,9 +56,15 @@ def readTimeInputsPerSpecies(inputFileData,inputFileMC):
                 correctionsData[iBin-1] = correctionsData[minBinsData[species]-1]-(correctionsMC[minBinsData[species]-1]-correctionsMC[iBin-1])
                 sigmas.append(sigmasHist.GetBinContent(minBinsData[species]))
             else:
-                sigmas.append(sigmasHist.GetBinContent(iBin))
+                if iBin > maxBinsData[species]:
+                    sigmas.append(sigmasHist.GetBinContent(maxBinsData[species]))
+                else:
+                    sigmas.append(sigmasHist.GetBinContent(iBin))
         timeCorrections["data",species] = (areaBins,correctionsData,[0]*len(sigmas))
         timeCorrections["signal",species] = (areaBins,correctionsMC,sigmas)
+        print (species)
+        for i in range(len(timeCorrections["data",species][0])):
+            print (i+1,timeCorrections["data",species][0][i],timeCorrections["data",species][1][i],timeCorrections["signal",species][1][i],timeCorrections["signal",species][2][i])
     return timeCorrections
 def readTimeInputs(inputFileDataAbs,inputFileMCAbs,inputFileDataRMS,inputFileMCRMS):
     #return time corrections from file (NB: should added to recorded time!)
@@ -80,9 +98,9 @@ def getCorrections(inputDir=None,saveToPickle=False,readFromPickle=None):
         return pickle.load(open(readFromPickle,'rb'))
     else:
         if inputDir == None:
-            inputDir = "./timeCorrectionsFiles_update/"
-            inputFileData = r.TFile(inputDir+"SmallPulseDelay_data.root")
-            inputFileMC = r.TFile(inputDir+"SmallPulseDelay_MC.root")
+            inputDir = "./timeCorrectionsFiles_update/V2_sat/"
+            inputFileData = r.TFile(inputDir+"SmallPulseDelay_data_Sat_noCh4.root")
+            inputFileMC = r.TFile(inputDir+"SmallPulseDelay_MC_Sat.root")
             allInputFiles = [inputFileData,inputFileMC]
             timeCorrections = readTimeInputsPerSpecies(inputFileData,inputFileMC)
         #old inputs
@@ -103,7 +121,4 @@ def getCorrections(inputDir=None,saveToPickle=False,readFromPickle=None):
 if __name__=="__main__":
     getCorrections(saveToPickle="timeCorrectionsPickle.pkl")
 
-if __name__=="__main__":
-    getCorrections()
-    
 

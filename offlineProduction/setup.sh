2ccb62b425adff503de12a1fb65346b29e2c4322
milliqan <milliqan@milliqandaq-00.dyndns.cern.ch>
2022-08-18 23:07:25 +0200

    alter setup to fix errors

--- offlineProduction/setup.sh
+++ offlineProduction/setup.sh
@@ -1,5 +1,6 @@
 #Change these as appropriate
 export MILLIDAQDIR="/home/milliqan/MilliDAQ/"
+export ROOT_INCLUDE_PATH=$MILLIDAQDIR/interface/
 export OFFLINESITE="CERN"
 export OFFLINEDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 

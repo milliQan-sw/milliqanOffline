a41008c1904a038d502bc41539a2eaaeccb5ce84
carriganm95 <carriganm95@gmail.com>
2022-10-12 18:19:18 -0400

    Update createContainer.sh
    updates to compile with singularity exec

--- singularity/createContainer.sh
+++ singularity/createContainer.sh
@@ -26,10 +26,4 @@ fi
 
 singularity build --fakeroot --sandbox $container mQContainer.def
 
-cd $container/milliqanOffline/offlineProduction
-
-sed -i 's|/home/milliqan/MilliDAQ/|$PWD/../../MilliDAQ|' setup.sh
-
-source setup.sh
-
-bash compile.sh ${executable}.exe
+singularity exec $container bash -c "cd $container/milliqanOffline/offlineProduction && echo $PWD && sed -i 's|/home/milliqan/MilliDAQ/|$PWD/../../MilliDAQ|' setup.sh && source setup.sh && bash compile.sh ${executable}.exe"

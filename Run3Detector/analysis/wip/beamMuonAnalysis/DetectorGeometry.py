7c75b34451e2f1bb62880c3f87583fefbe20b785
CollinKa <colin.ka9@gmail.com>
2023-03-15 20:37:21 -0400

    add the analysis file for beam muon eveent

---
+++ Run3Detector/analysis/wip/beamMuonAnalysis/DetectorGeometry.py
@@ -0,0 +1,6 @@
+# Variables that define the geometry of MilliQan detector
+vetoPanels = [68, 69, 70, 72, 73, 74]
+nPEthreashold = 60
+timebL =15 #in the unit of ns
+NumOfbigpulse = 7 #number of pulse that is larger than
+pulseHeight = 500 #threashold value

4c31259e4244805a19765cf9b7bad6628a423a89
carriganm95 <carriganm95@gmail.com>
2023-07-17 15:55:32 -0400

    Delete processRun.sh

--- Run3Detector/processRun.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-run=66
-
-for i in {0..12}
-do
-    python3 scripts/runOfflineFactory.py --inputFile /store/user/milliqan/run3/0/0006/MilliQanSlab_Run${run}.${i}_default.root --outputFile MilliQanSlab_Run${run}.${i}_fullProcessed.root --exe ./run.exe 
-done

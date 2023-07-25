0cc4c8b2c7910ae757e7909d7a70382d482fab6e
Ryan De Los Santos <ryansantos1174@gmail.com>
2023-07-25 10:29:10 -0400

    Create python directory and removed pmt_calibration duplicate

--- Run3Detector/analysis/pmt_calibration/python/TemplateTools.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-# Input a list of lists
-def averageOfLists(otherLists : list ) -> list:
-    sumList =  [sum(x) for x in zip(*otherLists)]
-    print(len(otherLists))
-    return [ x/len(otherLists) for x in sumList ]

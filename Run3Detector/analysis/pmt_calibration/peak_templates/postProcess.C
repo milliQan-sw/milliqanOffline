770a6d3679399ffddcb82acf2df0b86e3a0fa83d
Ryan De Los Santos <ryansantos1174@gmail.com>
2023-08-05 22:07:59 -0400

    Update to postProcess.C to loop over all events

--- Run3Detector/analysis/pmt_calibration/peak_templates/postProcess.C
+++ Run3Detector/analysis/pmt_calibration/peak_templates/postProcess.C
@@ -3,6 +3,7 @@
 #include <TFile.h>
 #include <TH1D.h>
 #include <TTree.h>
+#include <algorithm>
 #include <cassert>
 #include <cmath>
 #include <iostream>
@@ -22,11 +23,12 @@ const double NANOSECONDS_PER_SAMPLE = 2.5;
 
 int postProcess() {
   bool do_timing = false;
-  int t_start = 230;
-  int t_end = 330;
+  int t_start = 1220;
+  int t_end = 1550;
   std::cout << "Test" << std::endl;
-  TFile *input_file =
-      new TFile("/home/ryan/Documents/Research/MilliQan/Data/run733.root");
+  TFile *input_file = new TFile("/home/ryan/Documents/Research/MilliQan/"
+                                "DataFiles/Run811preProcessed.root",
+                                "UPDATE");
   if (!(input_file->IsZombie())) {
     TTree *event_tree = (TTree *)input_file->Get("Events");
     std::cout << "After getting Event branch" << std::endl;
@@ -48,12 +50,6 @@ int postProcess() {
     event_tree->SetBranchAddress("times", &times);
     event_tree->SetBranchAddress("voltages", &voltages);
 
-    // for (Long64_t i = 0; i < event_tree->GetEntries(); i++) {
-    //   event_tree->GetEntry(i);
-    //   std::cout << voltages->size() << std::endl;
-    // }
-
-    event_tree->GetEntry(0);
     TBranch *area_branch = event_tree->Branch("area", &area, "area/D");
     TBranch *offset_branch = event_tree->Branch("offset", &offset, "area/D");
     TBranch *noise_branch = event_tree->Branch("noise", &noise, "area/D");
@@ -73,37 +69,44 @@ int postProcess() {
     double xq[2] = {0.95, 0.05};
     double yq[2];
     int offset_voltage_cutoff = start_index * 3. / 4.;
-    std::vector<double> sliced_voltages =
-        slice_vector(*voltages, 30, offset_voltage_cutoff - 1);
-    offset[0] = mean(sliced_voltages);
 
-    TH1F *h = new TH1F("h", "voltage_quantile", 100, -20, 20);
-    std::vector<double> noise_voltage_slice =
-        slice_vector(*voltages, 0, offset_voltage_cutoff - 1);
-    std::vector<double> w(noise_voltage_slice.size(), 1); // weights vector
-    h->FillN(noise_voltage_slice.size(), noise_voltage_slice.data(), w.data());
-    h->Draw();
-    h->GetQuantiles(2, yq, xq);
-    std::cout << "0.95 Quantile " << yq[0] << std::endl;
-    std::cout << "0.05 Quantile " << yq[1] << std::endl;
+    for (int i = 0; i < event_tree->GetEntries(); i++) {
+      event_tree->GetEntry(i);
 
-    std::cout << "Length Noise " << noise_voltage_slice.size() << std::endl;
+      // // Multiply voltages by -1
+      // for (auto &voltage : *voltages) {
+      //   voltage *= -1;
+      // }
 
-    // Noise is a little off
-    noise[0] = 0.5 * (yq[0] - yq[1]);
-    std::cout << "Noise " << noise[0] << std::endl;
-    std::cout << "Offset " << offset[0] << std::endl;
-    for (auto &voltage : *voltages) {
-      voltage -= offset[0];
+      std::vector<double> sliced_voltages =
+          slice_vector(*voltages, 30, offset_voltage_cutoff - 1);
+      offset[0] = mean(sliced_voltages);
+
+      TH1F *h = new TH1F("h", "voltage_quantile", 100, -20, 20);
+      std::vector<double> noise_voltage_slice =
+          slice_vector(*voltages, 0, offset_voltage_cutoff - 1);
+      std::vector<double> w(noise_voltage_slice.size(), 1); // weights vector
+      h->FillN(noise_voltage_slice.size(), noise_voltage_slice.data(),
+               w.data());
+      h->GetQuantiles(2, yq, xq);
+      delete h;
+      // Noise is a little off
+      noise[0] = 0.5 * (yq[0] - yq[1]);
+      std::cout << "Noise " << noise[0] << std::endl;
+      std::cout << "Offset " << offset[0] << std::endl;
+      for (auto &voltage : *voltages) {
+        voltage -= offset[0];
+      }
+      // NOTE: trapezoid rule works!
+      area[0] = trapezoid_rule_area(*times, *voltages);
+      area_branch->Fill();
+      offset_branch->Fill();
+      noise_branch->Fill();
     }
-    // NOTE: trapezoid rule works!
-    std::vector<double> x_test = {1, 2, 3, 4, 5, 6, 7, 23, 233};
-    std::vector<double> y_test = {4, 3, 65, 2, 3, 5, 3, 74, 345};
-    double area_test = trapezoid_rule_area(x_test, y_test);
-    std::cout << area_test << std::endl; // 163
+    event_tree->Write("", TObject::kOverwrite);
     input_file->Close();
-    return 0;
   }
+  return 0;
 }
 
 double mean(std::vector<double> values) {
@@ -121,7 +124,7 @@ std::vector<T> slice_vector(std::vector<T> input_vector, int start_value,
   auto start = input_vector.begin() + start_value;
   auto end = input_vector.begin() + stop_value + 1;
 
-  std::cout << input_vector.size() << std::endl;
+  std::cout << "Input vector size" << input_vector.size() << std::endl;
   std::vector<T> result(stop_value - start_value + 1);
   copy(start, end, result.begin());
   std::cout << "result_size" << result.size() << std::endl;

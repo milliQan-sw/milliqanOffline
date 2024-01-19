a306f5bb9363bd50b7510580987833e61d2264af
carriganm95 <carriganm95@gmail.com>
2024-01-19 15:28:09 +0100

    build analysis CI

---
+++ analysisCI.py
@@ -0,0 +1,8 @@
+import os
+import sys
+sys.path.append(os.getcwd() + '/Run3Detector/analysis/utilities/')
+print(sys.path)
+from milliqanProcessor import *
+from milliqanScheduler import *
+from milliqanCuts import *
+from milliqanPlotter import *

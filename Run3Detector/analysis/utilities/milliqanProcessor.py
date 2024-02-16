d213e38251110231a0147248d927463dc28edb80
Jacob Steenis <86564501+steenisj@users.noreply.github.com>
2024-02-16 21:29:36 +0100

    Steenis dev (#79)
    * Added a script to explore time walk effects. Also modified Plot() in milliqanProcessor to be able to generate 2d and 3d histograms.
    Rebased to master prior to pushing my changes to milliqan plotter and milliqan processor
    
    * Removed unfinished timeWalk.py to make a pull request
    
    * Removed lines for 3d histogram capability in milliqanPlotter because FillN() does not work for 3d histograms
    
    * committing prior to trying to make a push with 2d histo changes
    
    * Rebased to master and included 2d histogram generation script along with the capabilities in plotter and processor
    
    ---------
    
    Co-authored-by: Jacob Steenis <jahe0@MacBook-Air-40.local>
    Co-authored-by: Jacob Steenis <jahe0@visitor-120650529.dyndns.cern.ch>

--- Run3Detector/analysis/utilities/milliqanProcessor.py
+++ Run3Detector/analysis/utilities/milliqanProcessor.py
@@ -30,12 +30,20 @@ class milliqanProcessor():
         self.mqSchedule.setEvents(events)
         for branch in self.mqSchedule.schedule:
             if isinstance(branch, milliqanPlot):
-                if branch.variables in events.fields:
+                if isinstance(branch.variables, list):
+                    for i in branch.variables:
+                        if i not in events.fields:
+                            print("Branch {0} does not exist in event array".format(i))
+                            break
                     branch.plot(events)
-                #elif branch.variables in self.custom_out:
-                #    branch.plot(self.custom_out)
                 else:
-                    print("Branch {0} does not exist in event array or custom output".format(branch.variables))
+                    if branch.variables in events.fields:
+                        branch.plot(events)
+                    #elif branch.variables in self.custom_out:
+                    #    branch.plot(self.custom_out)
+                    else:
+                        print("Branch {0} does not exist in event array or custom output".format(branch.variables))
+                        break
             else:
                 branch()
         return events
@@ -81,5 +89,5 @@ class milliqanProcessor():
             
             if hasattr(self, 'customFunction'):
                 self.custom_out = self.runCustomFunction(events)
-
+    
         print("Number of processed events", total_events)

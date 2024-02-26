f9ca6a971ed2bcfe3b4842c2f846d5f7dbccedc4
CollinKa <colin.ka9@gmail.com>
2024-02-25 19:07:45 -0500

    panel hit should be top panel hit(row==4)

--- Run3Detector/analysis/utilities/CosmicmuonTag_stable.py
+++ Run3Detector/analysis/utilities/CosmicmuonTag_stable.py
@@ -17,7 +17,7 @@ import numpy as np
 
 from CosmicTagPlots import plots
 
-#filelist =['/mnt/hadoop/se/store/user/milliqan/trees/v34/MilliQan_Run1190.4_v34.root:t']
+#filelist =['/mnt/hadoop/se/store/user/milliqan/trees/v34/1100/MilliQan_Run1190.4_v34.root:t']
 #runN = 1190
 #"""
 runN = 1190
@@ -34,13 +34,17 @@ cosmicGoodRun = [runN]
 for run in cosmicGoodRun:
     appendRun(filelist,run)
 #"""
-pulseBasedBranches = ["pickupFlag","chan","layer","nPE","type","row"]
-branches = ["chan","runNumber","event","fileNumber",'boardsMatched',"pickupFlag","layer","nPE","type","row"]
+
+branches = ["chan","runNumber","event","fileNumber",'boardsMatched',"pickupFlag","layer","nPE","type","row","area"]
 NPECut = 20
 ChanVsbarNpeBTag1 = r.TH2F("B ChanvsNPE tag 1","bar chanvsmpe tag1;chan; pulse NPE", 80,0,80,200,0,1000)
 ChanVsbarNpePTag1 = r.TH2F("P ChanvsNPE tag 1","panel chanvsmpe tag1;chan; pulse NPE", 80,0,80,200,0,1000)
 ChanVsbarNpeBTag2 = r.TH2F("B ChanvsNPE tag 2","bar chanvsmpe tag2;chan; pulse NPE", 80,0,80,200,0,1000)
 ChanVsbarNpePTag2 = r.TH2F("P ChanvsNPE tag 2","panel chanvsmpe tag2;chan; pulse NPE", 80,0,80,200,0,1000)
+ChanVsbarNpeBTag3 = r.TH2F("B ChanvsNPE tag 3","bar chanvsmpe tag3;chan; pulse NPE", 80,0,80,200,0,1000)
+ChanVsbarNpePTag3 = r.TH2F("P ChanvsNPE tag 3","panel chanvsmpe tag3;chan; pulse NPE", 80,0,80,200,0,1000)
+ChanVsbarNpeBTag4 = r.TH2F("B ChanvsNPE tag 4","bar chanvsmpe tag4;chan; pulse NPE", 80,0,80,200,0,1000)
+ChanVsbarNpePTag4 = r.TH2F("P ChanvsNPE tag 4","panel chanvsmpe tag4;chan; pulse NPE", 80,0,80,200,0,1000)
 
 
 for events in uproot.iterate(
@@ -51,14 +55,18 @@ for events in uproot.iterate(
     ):
     #total_events += len(events)
     events['boardsMatched'], junk = ak.broadcast_arrays(events.boardsMatched, events.pickupFlag)
-    
+    pulseBasedBranches = ["pickupFlag","chan","layer","nPE","type","row","area"] 
+    #"""
     for branch in pulseBasedBranches:
         events[branch] = events[branch][events.boardsMatched]
     for branch in pulseBasedBranches:
-        events[branch] = events[branch][events.pickupFlag]
-    barCut=events['type']==0
-    for branch in pulseBasedBranches:
-        events[branch] = events[branch][barCut]
+        events[branch] = events[branch][~events.pickupFlag]
+    #"""
+    events["barCut"]=events['type']==0
+    pulseBasedBranches.append("barCut")
+
+    #for branch in pulseBasedBranches:
+    #    events[branch] = events[branch][barCut]
     
     NPECuts = events.nPE >= NPECut
     for branch in pulseBasedBranches:
@@ -66,7 +74,7 @@ for events in uproot.iterate(
     
     for R in range(4):
         for l in range(4):
-            events[f"l{l}R{R}"] = (events.layer == l) & (events.row == R)
+            events[f"l{l}R{R}"] = (events.layer == l) & (events.row == R) & events.barCut
     
     
     events["fourRowBigHits"] = (ak.any(events.l0R0==True, axis=1) & 
@@ -83,13 +91,23 @@ for events in uproot.iterate(
                                 ak.any(events.l3R2==True, axis=1) & 
                                 ak.any(events.l3R3==True, axis=1)) 
 
-    #top and bottom row has big hit
+    #top and bottom row has big hits
     events["TBBigHit"] = (ak.any(events.l0R0==True, axis=1) & 
                                 ak.any(events.l0R3==True, axis=1)) | (ak.any(events.l1R0==True, axis=1) &  
                                 ak.any(events.l1R3==True, axis=1)) | (ak.any(events.l2R0==True, axis=1) & 
                                 ak.any(events.l2R3==True, axis=1)) | (ak.any(events.l3R0==True, axis=1) & 
                                 ak.any(events.l3R3==True, axis=1)) 
-    
+    #Top cosmic panel + row 0 bar channel have big hits
+    events["panel"]=events['row']==4
+    events["estimatedNPE"] = events.area / 1320
+    events["panelHit"] = ak.any((events.estimatedNPE >= 20) & (events.panel), axis = 1)
+    #strict cosmic muon with panel tag 
+    events["StPanelTag"] = events["panelHit"] & events["TBBigHit"]
+    #sufficienc cosmic muon with panel tag 
+    events["SuPanelTag"] = events["panelHit"] & ak.any((events["row"] == 0) & (events["type"] == 0),axis =1)
+
+
+
 
     #debug
     #print(ak.to_pandas(events[events.TBBigHit]))
@@ -105,7 +123,6 @@ for events in uproot.iterate(
     #The head(max) of NPE distribution is for cosmic muon and the tail is for low energy photon 
     #there is need to get the origianl event since I used bar & NPE trim
     for RN,FN,EV in zip(runNumberList,FileNumberList,EventIDlist):
-       
         plots(RN,FN,EV,ChanVsbarNpeBTag1,ChanVsbarNpePTag1)
     
 
@@ -115,14 +132,33 @@ for events in uproot.iterate(
     EventIDlist2 = (ak.to_list(events["event"][events.fourRowBigHits == True]))
     for RN,FN,EV in zip(runNumberList2,FileNumberList2,EventIDlist2):
         plots(RN,FN,EV,ChanVsbarNpeBTag2,ChanVsbarNpePTag2)
+
+
+    FileNumberList3=(ak.to_list(events["fileNumber"][events.StPanelTag == True]))
+    runNumberList3=(ak.to_list(events["runNumber"][events.StPanelTag == True]))
+    EventIDlist3 = (ak.to_list(events["event"][events.StPanelTag == True]))
+    for RN,FN,EV in zip(runNumberList3,FileNumberList3,EventIDlist3):
+        plots(RN,FN,EV,ChanVsbarNpeBTag3,ChanVsbarNpePTag3)
+
+
+    FileNumberList4=(ak.to_list(events["fileNumber"][events.SuPanelTag == True]))
+    runNumberList4=(ak.to_list(events["runNumber"][events.SuPanelTag == True]))
+    EventIDlist4 = (ak.to_list(events["event"][events.SuPanelTag == True]))
+    for RN,FN,EV in zip(runNumberList4,FileNumberList4,EventIDlist4):
+        plots(RN,FN,EV,ChanVsbarNpeBTag4,ChanVsbarNpePTag4)
     
     
 
 
-output_file = r.TFile(f"Run{runN}chanvsNPE_NoPickUPboardmaChecked.root", "RECREATE")
+#output_file = r.TFile(f"Run{runN}chanvsNPE_NoPickUPboardmaChecked.root", "RECREATE")
+output_file = r.TFile(f"Run{runN}chanvsNPE.root", "RECREATE")
 ChanVsbarNpeBTag1.Write()
 ChanVsbarNpePTag1.Write()
 ChanVsbarNpeBTag2.Write()
 ChanVsbarNpePTag2.Write()
+ChanVsbarNpeBTag3.Write()
+ChanVsbarNpePTag3.Write()
+ChanVsbarNpeBTag4.Write()
+ChanVsbarNpePTag4.Write()
 output_file.Close()
 

11732e00f09a94492f84b8c3b4a30182de52a821
Collin Zheng <zheng.1947@osu.edu>
2024-03-08 11:53:58 -0500

    counting script for cosmic muon tagging cut efficiency check

---
+++ Run3Detector/analysis/simAnalysis/summingResult_cuteff.py
@@ -0,0 +1,51 @@
+#this file is made for doing the cut efficiency analysis for study the cosmic muon tagging algorism
+
+import os
+
+event_sums = {}
+
+input_files = []
+
+directory= "/home/czheng/SimCosmicFlatTree/cutEffCheckResult/"
+
+base_name = "Run"
+
+for filename in os.listdir(directory):
+    if filename.startswith(base_name) and filename.endswith("Flow3.txt"):
+        input_files.append(directory+filename)
+
+interestFile = list()
+
+#print(input_files)
+files = 0
+for filepath in input_files:
+    if not os.path.exists(filepath):
+        continue
+    interestFile.append(filepath)
+    with open(filepath, 'r') as file:
+        lines = file.readlines()
+        files += 1
+        for line in lines:
+            if ':' in line:
+                parts = line.split(':')
+
+                
+                event = parts[0]
+
+                
+                #print(event)
+                
+                count = int(parts[1])
+                #print(count)
+                #count = int(count.strip())
+                # update the count in the dictionary
+                if event in event_sums:
+                    event_sums[event] += count
+                else:
+                    event_sums[event] = count
+
+print(len(interestFile))      
+for event, count in event_sums.items():
+    print(f"{event}: {count}")
+
+#print(files)

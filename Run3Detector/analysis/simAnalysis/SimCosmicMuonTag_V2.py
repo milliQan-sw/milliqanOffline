f24c5c467826f2a554ca4c15633b0201921218ba
Collin Zheng <61294685+CollinKa@users.noreply.github.com>
2024-03-13 00:00:48 -0400

    change EmptyListFilter() and create simbar and simpanel cut

--- Run3Detector/analysis/simAnalysis/SimCosmicMuonTag_V2.py
+++ Run3Detector/analysis/simAnalysis/SimCosmicMuonTag_V2.py
@@ -29,29 +29,6 @@ myplotter = milliqanPlotter()
 
 #histograms for different cosmic muon tags
 
-ChanVsbarNpeBTag1 = r.TH2F("ChanVsbarNpeBTag1","bar chanvsmpe tag1;chan; bar NPE", 80,0,80,200,0,100000)
-ChanVsbarNpePTag1 = r.TH2F("P ChanvsNPE tag 1","panel chanvsmpe tag1;chan; bar NPE", 80,0,80,200,0,100000)
-NBarsHitTag1 =  r.TH1F("NBarsHitTag1" , "number of bars get hit;number of bars; Events",60,0,60)
-CorrectTimeDtTag1 =  r.TH1F("CorrectTimeDtTag1" , "D_t Max with correction w;D_t Max; Events",40,-15,25)
-NPERatioTag1 = r.TH1F("NPEratioTag1","NPE ratio;max NPE/min NPE;Events",150,0,150)
-
-ChanVsbarNpeBTag2 = r.TH2F("B ChanvsNPE tag 2","bar chanvsmpe tag2;chan; bar NPE", 80,0,80,200,0,100000)
-ChanVsbarNpePTag2 = r.TH2F("P ChanvsNPE tag 2","panel chanvsmpe tag2;chan; bar NPE", 80,0,80,200,0,100000)
-NBarsHitTag2 =  r.TH1F("NBarsHitTag2" , "number of bars get hit;number of bars; Events",60,0,60)
-CorrectTimeDtTag2 =  r.TH1F("CorrectTimeDtTag2" , "D_t Max with correction w;D_t Max; Events",40,-15,25)
-NPERatioTag2 = r.TH1F("NPEratioTag2","NPE ratio;max NPE/min NPE;Events",150,0,150)
-
-ChanVsbarNpeBTag3 = r.TH2F("B ChanvsNPE tag 3","bar chanvsmpe tag3;chan; bar NPE", 80,0,80,200,0,1000)
-ChanVsbarNpePTag3 = r.TH2F("P ChanvsNPE tag 3","panel chanvsmpe tag3;chan; bar NPE", 80,0,80,200,0,1000)
-NBarsHitTag3 =  r.TH1F("NBarsHitTag3" , "number of bars get hit;number of bars; Events",60,0,60)
-CorrectTimeDtTag3 =  r.TH1F("CorrectTimeDtTag3" , "D_t Max with correction w;D_t Max; Events",40,-15,25)
-NPERatioTag3 = r.TH1F("NPEratioTag3","NPE ratio;max NPE/min NPE;Events",150,0,150)
-
-ChanVsbarNpeBTag4 = r.TH2F("B ChanvsNPE tag 4","bar chanvsmpe tag4;chan; bar NPE", 80,0,80,200,0,1000)
-ChanVsbarNpePTag4 = r.TH2F("P ChanvsNPE tag 4","panel chanvsmpe tag4;chan; bar NPE", 80,0,80,200,0,1000)
-NBarsHitTag4 =  r.TH1F("NBarsHitTag4" , "number of bars get hit;number of bars; Events",60,0,60)
-CorrectTimeDtTag4 =  r.TH1F("CorrectTimeDtTag4" , "D_t Max with correction w;D_t Max; Events",40,-15,25)
-NPERatioTag4 = r.TH1F("NPEratioTag4","NPE ratio;max NPE/min NPE;Events",150,0,150)
 
 
 
@@ -196,31 +173,7 @@ def LayerContraint(self,layer0Cut,layer1Cut,layer2Cut,layer3Cut, layerConstraint
 
 
 
-myplotter.addHistograms(NBarsHitTag1, 'NBarsHits', 'fourRowBigHits')
 
-myplotter.addHistograms(NBarsHitTag2, 'NBarsHits', 'TBBigHit')
-
-myplotter.addHistograms(NBarsHitTag3, 'NBarsHits', 'P_TBBigHit')
-
-myplotter.addHistograms(NBarsHitTag4, 'NBarsHits', 'P_BBigHit')
-
-
-myplotter.addHistograms(NPERatioTag1, 'BarNPERatio', 'fourRowBigHits')
-
-myplotter.addHistograms(NPERatioTag2, 'BarNPERatio', 'TBBigHit')
-
-myplotter.addHistograms(NPERatioTag3, 'BarNPERatio', 'P_TBBigHit')
-
-myplotter.addHistograms(NPERatioTag4, 'BarNPERatio', 'P_BBigHit')
-
-
-myplotter.addHistograms(CorrectTimeDtTag1, 'DT_CorrectTime', 'fourRowBigHits')
-
-myplotter.addHistograms(CorrectTimeDtTag2, 'DT_CorrectTime', 'TBBigHit')
-
-myplotter.addHistograms(CorrectTimeDtTag3, 'DT_CorrectTime', 'P_TBBigHit')
-
-myplotter.addHistograms(CorrectTimeDtTag4, 'DT_CorrectTime', 'P_BBigHit')
 
 
 
@@ -397,8 +350,8 @@ def P_BBigHit(self, cutName = None,cut = None):
 #remove the empty events
 def EmptyListFilter(self,cutName=None):
 
-    self.events['None_empty_event'] = ak.num(self.events['layer']) > 0
-    self.events=self.events[self.events.None_empty_event]
+    self.events['None_empty_event'] = ak.num(self.events['layer']) > 0 #create a event-based mask that check if the event is empty
+    #self.events=self.events[self.events.None_empty_event]#this one can cause milliqanplotter unable to work. I haven't figure out the cuase yet.
 
 
 #tag muon event (sim only)
@@ -413,7 +366,8 @@ def MuonEvent(self, cutName = None, CutonBars = True, branches = None):
     else:
         self.events = self.events[ak.any(self.events.muonHit == 1, axis = 1)]
 
-def countEvent(self, cutName = None, Countobject=None, debug = False):
+#the default behaviour is to count the number of events that has data.
+def countEvent(self, cutName = None, Countobject='None_empty_event', debug = False):
     if debug: 
         print(ak.to_pandas(self.events))
     if cutName:
@@ -422,6 +376,20 @@ def countEvent(self, cutName = None, Countobject=None, debug = False):
         print(f"current available events : {len(self.events['event'])}")
 
 
+#the EmptyListFilter() has a weird bug that can cause the milliqanplotter unable to work. So the current solution for doing analysis without looping over the empty 
+#FIXME: check does it work? if not then use the milliqancut
+def SimPanelCut(self):
+    PanelDefault=self.events['type'] == 2
+    self.events['barCut'] = PanelDefault & self.events["None_empty_event"]
+
+def SimBarCut(self):
+    barCutdefault=self.events['type'] == 0
+    self.events['barCut'] = barCutdefault & self.events["None_empty_event"]
+
+
+setattr(milliqanCuts, 'SimPanelCut', SimPanelCut)
+
+setattr(milliqanCuts, 'SimBarCut', SimBarCut)
 
 setattr(milliqanCuts, 'countEvent', countEvent)
 
@@ -511,6 +479,7 @@ if __name__ == "__main__":
 
     appendRun(filelist)
     """
+    #---------------------------------------------------------------------------------------------
     #branch for data analysis
     branches = ["chan","runNumber","event","layer","nPE","type","row","muonHit"]
 
@@ -535,7 +504,66 @@ if __name__ == "__main__":
     #NbarsHitsCount1= mycuts.getCut(mycuts.P_BBigHit, "NBarsHits",cut = None,hist = NBarsHitTag1)#FIXME: getCut can't take hist as argument. Maybe I should remove it
     #cutflowSTD = [mycuts.MuonEvent,mycuts.EmptyListFilter,mycuts.countEvent,mycuts.barCut,mycuts.panelCut,mycuts.CosmuonTagIntialization,TBBigHitCut,mycuts.NbarsHitsCount ,myplotter.dict['NBarsHitTag2']] #default analysis cutflow
 
+    #-------------------------section for making histograms---------------------------------------
+    ChanVsbarNpeBTag1 = r.TH2F("ChanVsbarNpeBTag1","bar chanvsmpe tag1;chan; bar NPE", 80,0,80,200,0,100000)
+    ChanVsbarNpePTag1 = r.TH2F("P ChanvsNPE tag 1","panel chanvsmpe tag1;chan; bar NPE", 80,0,80,200,0,100000)
+    NBarsHitTag1 =  r.TH1F("NBarsHitTag1" , "number of bars get hit;number of bars; Events",60,0,60)
+    CorrectTimeDtTag1 =  r.TH1F("CorrectTimeDtTag1" , "D_t Max with correction w;D_t Max; Events",40,-15,25)
+    NPERatioTag1 = r.TH1F("NPEratioTag1","NPE ratio;max NPE/min NPE;Events",150,0,150)
 
+    ChanVsbarNpeBTag2 = r.TH2F("B ChanvsNPE tag 2","bar chanvsmpe tag2;chan; bar NPE", 80,0,80,200,0,100000)
+    ChanVsbarNpePTag2 = r.TH2F("P ChanvsNPE tag 2","panel chanvsmpe tag2;chan; bar NPE", 80,0,80,200,0,100000)
+    NBarsHitTag2 =  r.TH1F("NBarsHitTag2" , "number of bars get hit;number of bars; Events",60,0,60)
+    CorrectTimeDtTag2 =  r.TH1F("CorrectTimeDtTag2" , "D_t Max with correction w;D_t Max; Events",40,-15,25)
+    NPERatioTag2 = r.TH1F("NPEratioTag2","NPE ratio;max NPE/min NPE;Events",150,0,150)
+
+    ChanVsbarNpeBTag3 = r.TH2F("B ChanvsNPE tag 3","bar chanvsmpe tag3;chan; bar NPE", 80,0,80,200,0,1000)
+    ChanVsbarNpePTag3 = r.TH2F("P ChanvsNPE tag 3","panel chanvsmpe tag3;chan; bar NPE", 80,0,80,200,0,1000)
+    NBarsHitTag3 =  r.TH1F("NBarsHitTag3" , "number of bars get hit;number of bars; Events",60,0,60)
+    CorrectTimeDtTag3 =  r.TH1F("CorrectTimeDtTag3" , "D_t Max with correction w;D_t Max; Events",40,-15,25)
+    NPERatioTag3 = r.TH1F("NPEratioTag3","NPE ratio;max NPE/min NPE;Events",150,0,150)
+
+    ChanVsbarNpeBTag4 = r.TH2F("B ChanvsNPE tag 4","bar chanvsmpe tag4;chan; bar NPE", 80,0,80,200,0,1000)
+    ChanVsbarNpePTag4 = r.TH2F("P ChanvsNPE tag 4","panel chanvsmpe tag4;chan; bar NPE", 80,0,80,200,0,1000)
+    NBarsHitTag4 =  r.TH1F("NBarsHitTag4" , "number of bars get hit;number of bars; Events",60,0,60)
+    CorrectTimeDtTag4 =  r.TH1F("CorrectTimeDtTag4" , "D_t Max with correction w;D_t Max; Events",40,-15,25)
+    NPERatioTag4 = r.TH1F("NPEratioTag4","NPE ratio;max NPE/min NPE;Events",150,0,150)
+
+    #FIXME: for some unknow reason the code at below unable to work, since the mask I created can't be accessed by milliqan for some unknown reason. If I can't figure out the solution, then they should be deleted.
+    """
+    myplotter.addHistograms(NBarsHitTag1, 'NBarsHits', 'fourRowBigHits')
+
+    myplotter.addHistograms(NBarsHitTag2, 'NBarsHits', 'TBBigHit')
+
+    myplotter.addHistograms(NBarsHitTag3, 'NBarsHits', 'P_TBBigHit')
+
+    myplotter.addHistograms(NBarsHitTag4, 'NBarsHits', 'P_BBigHit')
+
+    myplotter.addHistograms(NPERatioTag1, 'BarNPERatio', 'fourRowBigHits')
+
+    myplotter.addHistograms(NPERatioTag2, 'BarNPERatio', 'TBBigHit')
+
+    myplotter.addHistograms(NPERatioTag3, 'BarNPERatio', 'P_TBBigHit')
+
+    myplotter.addHistograms(NPERatioTag4, 'BarNPERatio', 'P_BBigHit')
+
+
+    myplotter.addHistograms(CorrectTimeDtTag1, 'DT_CorrectTime', 'fourRowBigHits')
+
+    myplotter.addHistograms(CorrectTimeDtTag2, 'DT_CorrectTime', 'TBBigHit')
+
+    myplotter.addHistograms(CorrectTimeDtTag3, 'DT_CorrectTime', 'P_TBBigHit')
+
+    myplotter.addHistograms(CorrectTimeDtTag4, 'DT_CorrectTime', 'P_BBigHit')
+    """
+    #it seems currently creating a combined mask can retrieve the data
+    mask0 = mycuts.getCut(mycuts.combineCuts, 'mask0', ['fourRowBigHits'])
+    mask1 = mycuts.getCut(mycuts.combineCuts, 'mask1', ['TBBigHit'])
+    mask2 = mycuts.getCut(mycuts.combineCuts, 'mask2', ['P_TBBigHit'])
+    mask3 = mycuts.getCut(mycuts.combineCuts, 'mask3', ['P_BBigHit'])
+    
+
+    #-------------------------cutflows-----------------------------------------------------------
 
     #Cut flow 1. This one is for testing the cut efficiency of different tags. TB big hits - > TB + panel big hits 
     cutflow1 = [MuonCut,mycuts.EmptyListFilter,mycuts.countEvent,mycuts.barCut,mycuts.panelCut,mycuts.CosmuonTagIntialization,TBBigHitCut,TBBigHitCutCount,P_TBBigHitCut,P_TBBigHitCutCount]
@@ -561,7 +589,7 @@ if __name__ == "__main__":
     cutflow3 = [MuonCut,mycuts.EmptyListFilter,mycuts.countEvent,mycuts.barCut,mycuts.panelCut,mycuts.CosmuonTagIntialization,P_BBigHitCut,P_BBigHitCutCount,TBBigHitCut,P_TBBigHitCut,P_TBBigHitCutCount]
 
     #Cut flow 3. but the the muon cut change the muon event cut
-    cutflow3A = [MuonEventCut,mycuts.EmptyListFilter,mycuts.countEvent,mycuts.barCut,mycuts.panelCut,mycuts.CosmuonTagIntialization,P_BBigHitCut,P_BBigHitCutCount,TBBigHitCut,P_TBBigHitCut,P_TBBigHitCutCount]
+    cutflow3A = [MuonEventCut,mycuts.EmptyListFilter,mycuts.countEvent,mycuts.SimBarCut,mycuts.SimPanelCut,mycuts.CosmuonTagIntialization,P_BBigHitCut,P_BBigHitCutCount,TBBigHitCut,P_TBBigHitCut,P_TBBigHitCutCount]
 
     #cut flow 3. but the muon event cut are removed
     cutflow3B = [mycuts.EmptyListFilter,mycuts.countEvent,mycuts.barCut,mycuts.panelCut,mycuts.CosmuonTagIntialization,P_BBigHitCut,P_BBigHitCutCount,TBBigHitCut,P_TBBigHitCut,P_TBBigHitCutCount]

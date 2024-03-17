4c65b93d617059e9012657854a2a8de3e57d18ce
Collin Zheng <61294685+CollinKa@users.noreply.github.com>
2024-03-16 21:52:54 -0400

    process all with photon files

--- Run3Detector/analysis/simAnalysis/MQmachineRun_3A.sh
+++ Run3Detector/analysis/simAnalysis/MQmachineRun_3A.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for ((num = 1; num <= 100; num ++))
+for ((num = 101; num <= 1300; num ++))
 do
     python3 SimCosmicMuonTag_3A.py $num /home/czheng/SimCosmicFlatTree/cutEffCheckResult_sim_2500NPEcut
 done    

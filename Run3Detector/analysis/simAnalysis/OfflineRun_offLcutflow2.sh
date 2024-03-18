219aa3e8fe0d0bcea736c75f3a6f770d11853231
Collin Zheng <61294685+CollinKa@users.noreply.github.com>
2024-03-18 14:15:30 -0400

    update for run 1163

--- Run3Detector/analysis/simAnalysis/OfflineRun_offLcutflow2.sh
+++ Run3Detector/analysis/simAnalysis/OfflineRun_offLcutflow2.sh
@@ -2,5 +2,5 @@
 
 for ((num = 1; num <= 207; num ++))
 do
-    python3 OLCosmicMuonTag_V2_CF2.py 1190  $num /home/czheng/SimCosmicFlatTree/cutEffCheckResult_OffL_20NPEcut
+    python3 OLCosmicMuonTag_V2_CF2.py 1163  $num /home/czheng/SimCosmicFlatTree/cutEffCheckResult_OffL_20NPEcut
 done    

39b8a6e148bc4a9577cd019d2d20c05dabef649c
Collin Zheng <zheng.1947@osu.edu>
2024-03-20 14:06:44 -0400

    debug

--- Run3Detector/analysis/simAnalysis/SIMMQplotter_containtBeta.py
+++ Run3Detector/analysis/simAnalysis/SIMMQplotter_containtBeta.py
@@ -579,7 +579,7 @@ if __name__ == "__main__":
     #-------------------------start of cut efficiency analysis cutflows-----------------------------------------------------------
 
     #Cut flow 1. This one is for testing the cut efficiency of different tags. TB big hits - > TB + panel big hits 
-    cutflow1 = [mycuts.EmptyListFilter,mycuts.countEvent,mycuts.barCut,mycuts.panelCut,mycuts.CosmuonTagIntialization,TBBigHitCut,TBBigHitCutCount,mycuts.MiddleRow,eventCuts,eventCuts2,mycuts.CheckFieldName,myplotter.dict['nPEPlot'],myplotter.dict['middleRowNPE'],myplotter.dict['ChanVsbarNpeB']] 
+    cutflow1 = [mycuts.EmptyListFilter,mycuts.countEvent,mycuts.barCut,mycuts.panelCut,mycuts.CosmuonTagIntialization,TBBigHitCut,TBBigHitCutCount,mycuts.MiddleRow,eventCuts,eventCuts2,eventCuts3, eventCuts4, mycuts.CheckFieldName,myplotter.dict['nPEPlot'],myplotter.dict['middleRowNPE'],myplotter.dict['ChanVsbarNpeB']] 
 
     #cutflow1 = [mycuts.EmptyListFilter,mycuts.countEvent,mycuts.barCut,mycuts.panelCut,mycuts.CosmuonTagIntialization,TBBigHitCut,TBBigHitCutCount,eventCuts,mycuts.CheckFieldName,myplotter.dict['nPEPlot']]
     

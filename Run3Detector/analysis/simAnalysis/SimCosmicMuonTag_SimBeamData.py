42ae6bf326550f405c2c7f33b1f69af2aa969f36
Collin Zheng <61294685+CollinKa@users.noreply.github.com>
2024-03-27 13:58:32 -0400

    add cutflow 2 and 3 for beam analysis

--- Run3Detector/analysis/simAnalysis/SimCosmicMuonTag_SimBeamData.py
+++ Run3Detector/analysis/simAnalysis/SimCosmicMuonTag_SimBeamData.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     P_TBBigHitCutCount= mycuts.getCut(mycuts.countEvent,"placeholder"  ,Countobject = "P_TBBigHit")
     P_BBigHitCutCount= mycuts.getCut(mycuts.countEvent, "placeholder" ,Countobject = "P_BBigHit")
 
-    #when the intial mask is MuonEventCut in the cutflow, then use the combined cut(mask) at below to do the counting and making polt.
+    #when the intial mask is MuonEventCut(B type initial cut) in the cutflow, then use the combined cut(mask) at below to do the counting and making polt.
     TBBigHit_M = mycuts.getCut(mycuts.combineCuts, 'TBBigHit_M', ["TBBigHit", "muonEvent"])
     fourRowBigHits_M = mycuts.getCut(mycuts.combineCuts, 'fourRowBigHits_M', ["fourRowBigHits", "muonEvent"])
     P_TBBigHit_M = mycuts.getCut(mycuts.combineCuts, 'P_TBBigHit_M', ["P_TBBigHit", "muonEvent"])
@@ -62,6 +62,51 @@ if __name__ == "__main__":
 
     myplotter.addHistograms(ChanVsbarNpe_TBBigHit_1B, ['chan','nPE'], 'TBBigHit')
     myplotter.addHistograms(ChanVsbarNpe_P_TBBigHit_1B, ['chan','nPE'], 'P_TBBigHit')
+
+
+    #plot for cutflow 2_default
+    ChanVsbarNpe_TBBigHit_2D = r.TH2F("ChanVsbarNpe_TBBigHit_2D","bar chanvsmpe tag1;chan; bar NPE", 80,0,80,200,0,100000)
+    ChanVsbarNpe_P_TBBigHit_2D = r.TH2F("ChanVsbarNpe_P_TBBigHit_2D","bar chanvsmpe tag1;chan; bar NPE", 80,0,80,200,0,100000)
+
+    myplotter.addHistograms(ChanVsbarNpe_TBBigHit_2D, ['chan','nPE'], 'TBBigHit')
+    myplotter.addHistograms(ChanVsbarNpe_P_TBBigHit_2D, ['chan','nPE'], 'P_TBBigHit')
+
+    #plot for cutflow 2_A
+    ChanVsbarNpe_TBBigHit_2A = r.TH2F("ChanVsbarNpe_TBBigHit_2A","bar chanvsmpe tag1;chan; bar NPE", 80,0,80,200,0,100000)
+    ChanVsbarNpe_P_TBBigHit_2A = r.TH2F("ChanVsbarNpe_P_TBBigHit_2A","bar chanvsmpe tag1;chan; bar NPE", 80,0,80,200,0,100000)
+
+    myplotter.addHistograms(ChanVsbarNpe_TBBigHit_2A, ['chan','nPE'], 'TBBigHit_M')
+    myplotter.addHistograms(ChanVsbarNpe_P_TBBigHit_2A, ['chan','nPE'], 'P_TBBigHit_M')
+
+    #plot for cutflow 2_B
+    ChanVsbarNpe_TBBigHit_2B = r.TH2F("ChanVsbarNpe_TBBigHit_2B","bar chanvsmpe tag1;chan; bar NPE", 80,0,80,200,0,100000)
+    ChanVsbarNpe_P_TBBigHit_2B = r.TH2F("ChanVsbarNpe_P_TBBigHit_2B","bar chanvsmpe tag1;chan; bar NPE", 80,0,80,200,0,100000)
+
+    myplotter.addHistograms(ChanVsbarNpe_TBBigHit_2B, ['chan','nPE'], 'TBBigHit')
+    myplotter.addHistograms(ChanVsbarNpe_P_TBBigHit_2B, ['chan','nPE'], 'P_TBBigHit')
+
+
+    #plot for cutflow 3_default
+    ChanVsbarNpe_TBBigHit_3D = r.TH2F("ChanVsbarNpe_TBBigHit_3D","bar chanvsmpe tag1;chan; bar NPE", 80,0,80,200,0,100000)
+    ChanVsbarNpe_P_TBBigHit_3D = r.TH2F("ChanVsbarNpe_P_TBBigHit_3D","bar chanvsmpe tag1;chan; bar NPE", 80,0,80,200,0,100000)
+
+    myplotter.addHistograms(ChanVsbarNpe_TBBigHit_3D, ['chan','nPE'], 'TBBigHit')
+    myplotter.addHistograms(ChanVsbarNpe_P_TBBigHit_3D, ['chan','nPE'], 'P_TBBigHit')
+
+    #plot for cutflow 3_A
+    ChanVsbarNpe_TBBigHit_3A = r.TH2F("ChanVsbarNpe_TBBigHit_3A","bar chanvsmpe tag1;chan; bar NPE", 80,0,80,200,0,100000)
+    ChanVsbarNpe_P_TBBigHit_3A = r.TH2F("ChanVsbarNpe_P_TBBigHit_3A","bar chanvsmpe tag1;chan; bar NPE", 80,0,80,200,0,100000)
+
+    myplotter.addHistograms(ChanVsbarNpe_TBBigHit_3A, ['chan','nPE'], 'TBBigHit_M')
+    myplotter.addHistograms(ChanVsbarNpe_P_TBBigHit_3A, ['chan','nPE'], 'P_TBBigHit_M')
+
+    #plot for cutflow 3_B
+    ChanVsbarNpe_TBBigHit_3B = r.TH2F("ChanVsbarNpe_TBBigHit_3B","bar chanvsmpe tag1;chan; bar NPE", 80,0,80,200,0,100000)
+    ChanVsbarNpe_P_TBBigHit_3B = r.TH2F("ChanVsbarNpe_P_TBBigHit_3B","bar chanvsmpe tag1;chan; bar NPE", 80,0,80,200,0,100000)
+
+    myplotter.addHistograms(ChanVsbarNpe_TBBigHit_3B, ['chan','nPE'], 'TBBigHit')
+    myplotter.addHistograms(ChanVsbarNpe_P_TBBigHit_3B, ['chan','nPE'], 'P_TBBigHit')
+    
     
 
 
@@ -81,9 +126,34 @@ if __name__ == "__main__":
     cutflow1_B = [mycuts.EmptyListFilter,mycuts.countEvent,mycuts.barCut,mycuts.panelCut,mycuts.CosmuonTagIntialization,TBBigHitCut,TBBigHitCutCount,P_TBBigHitCut,P_TBBigHitCutCount,myplotter.dict['ChanVsbarNpe_TBBigHit_1B'],myplotter.dict['ChanVsbarNpe_P_TBBigHit_1B']]
 
 
+    cutflow2_D = [MuonCut,mycuts.EmptyListFilter,mycuts.countEvent,mycuts.barCut,mycuts.panelCut,mycuts.CosmuonTagIntialization,TBBigHitCut,TBBigHitCutCount,fourRowBigHitsCut,fourRowBigHitsCutCount,myplotter.dict['ChanVsbarNpe_TBBigHit_2D'],myplotter.dict['ChanVsbarNpe_P_TBBigHit_2D']]
+
+    cutflow2_A = [MuonEventCut,mycuts.EmptyListFilter,mycuts.countEvent,mycuts.barCut,mycuts.panelCut,mycuts.CosmuonTagIntialization,TBBigHitCut,TBBigHit_M,TBBigHitCutCount_m,fourRowBigHitsCut,fourRowBigHits_M,fourRowBigHitsCutCount_m,myplotter.dict['ChanVsbarNpe_TBBigHit_2A'],myplotter.dict['ChanVsbarNpe_P_TBBigHit_2A']]
+
+    cutflow2_B = [mycuts.EmptyListFilter,mycuts.countEvent,mycuts.barCut,mycuts.panelCut,mycuts.CosmuonTagIntialization,TBBigHitCut,TBBigHitCutCount,fourRowBigHitsCut,fourRowBigHitsCutCount,myplotter.dict['ChanVsbarNpe_TBBigHit_2B'],myplotter.dict['ChanVsbarNpe_P_TBBigHit_2B']]
+
+
+    cutflow3_D = [MuonCut,mycuts.EmptyListFilter,mycuts.countEvent,mycuts.barCut,mycuts.panelCut,mycuts.CosmuonTagIntialization,P_BBigHitCut,P_BBigHitCutCount,P_TBBigHitCut,P_TBBigHitCutCount,myplotter.dict['ChanVsbarNpe_TBBigHit_3D'],myplotter.dict['ChanVsbarNpe_P_TBBigHit_3D']]
+
+    cutflow3_A = [MuonEventCut,mycuts.EmptyListFilter,mycuts.countEvent,mycuts.barCut,mycuts.panelCut,mycuts.CosmuonTagIntialization,P_BBigHitCut,P_BBigHit_M,P_BBigHitCutCount_m,P_TBBigHitCut,P_TBBigHit_M,P_TBBigHitCutCount_m ,myplotter.dict['ChanVsbarNpe_TBBigHit_3A'],myplotter.dict['ChanVsbarNpe_P_TBBigHit_3A']]
+
+    cutflow3_B = [mycuts.EmptyListFilter,mycuts.countEvent,mycuts.barCut,mycuts.panelCut,mycuts.CosmuonTagIntialization,P_BBigHitCut,P_BBigHitCutCount,P_TBBigHitCut,P_TBBigHitCutCount,myplotter.dict['ChanVsbarNpe_TBBigHit_3B'],myplotter.dict['ChanVsbarNpe_P_TBBigHit_3B']]
+
+    
+
     cutflow1_D_dict = {'cutflow1_D': cutflow1_D}
     cutflow1_A_dict = {'cutflow1_A': cutflow1_A}
     cutflow1_B_dict = {'cutflow1_B': cutflow1_B}
+
+    cutflow2_D_dict = {'cutflow2_D': cutflow2_D}
+    cutflow2_A_dict = {'cutflow2_A': cutflow2_A}
+    cutflow2_B_dict = {'cutflow2_B': cutflow2_B}
+
+    cutflow3_D_dict = {'cutflow3_D': cutflow3_D}
+    cutflow3_A_dict = {'cutflow3_A': cutflow3_A}
+    cutflow3_B_dict = {'cutflow3_B': cutflow3_B}
+
+
 
     def analysis(cutflow_dict,fileName,Out_fileName="demo",*args):
 
@@ -110,7 +180,7 @@ if __name__ == "__main__":
 
 
    
-
+    """
     analysis(cutflow1_D_dict,fileName1,fileName1,ChanVsbarNpe_TBBigHit_1D,ChanVsbarNpe_P_TBBigHit_1D)
     analysis(cutflow1_D_dict,fileName2,fileName2,ChanVsbarNpe_TBBigHit_1D,ChanVsbarNpe_P_TBBigHit_1D)
     analysis(cutflow1_D_dict,fileName3,fileName3,ChanVsbarNpe_TBBigHit_1D,ChanVsbarNpe_P_TBBigHit_1D)
@@ -120,4 +190,35 @@ if __name__ == "__main__":
     analysis(cutflow1_B_dict,fileName1,fileName1,ChanVsbarNpe_TBBigHit_1B,ChanVsbarNpe_P_TBBigHit_1B)
     analysis(cutflow1_B_dict,fileName2,fileName2,ChanVsbarNpe_TBBigHit_1B,ChanVsbarNpe_P_TBBigHit_1B)
     analysis(cutflow1_B_dict,fileName3,fileName3,ChanVsbarNpe_TBBigHit_1B,ChanVsbarNpe_P_TBBigHit_1B)
+    """
+
+
+    analysis(cutflow2_D_dict,fileName1,fileName1,ChanVsbarNpe_TBBigHit_2D,ChanVsbarNpe_P_TBBigHit_2D)
+    analysis(cutflow2_D_dict,fileName2,fileName2,ChanVsbarNpe_TBBigHit_2D,ChanVsbarNpe_P_TBBigHit_2D)
+    analysis(cutflow2_D_dict,fileName3,fileName3,ChanVsbarNpe_TBBigHit_2D,ChanVsbarNpe_P_TBBigHit_2D)
+    analysis(cutflow2_A_dict,fileName1,fileName1,ChanVsbarNpe_TBBigHit_2A,ChanVsbarNpe_P_TBBigHit_2A)
+    analysis(cutflow2_A_dict,fileName2,fileName2,ChanVsbarNpe_TBBigHit_2A,ChanVsbarNpe_P_TBBigHit_2A)
+    analysis(cutflow2_A_dict,fileName3,fileName3,ChanVsbarNpe_TBBigHit_2A,ChanVsbarNpe_P_TBBigHit_2A)
+    analysis(cutflow2_B_dict,fileName1,fileName1,ChanVsbarNpe_TBBigHit_2B,ChanVsbarNpe_P_TBBigHit_2B)
+    analysis(cutflow2_B_dict,fileName2,fileName2,ChanVsbarNpe_TBBigHit_2B,ChanVsbarNpe_P_TBBigHit_2B)
+    analysis(cutflow2_B_dict,fileName3,fileName3,ChanVsbarNpe_TBBigHit_2B,ChanVsbarNpe_P_TBBigHit_2B)
+
+
+
+    analysis(cutflow3_D_dict,fileName1,fileName1,ChanVsbarNpe_TBBigHit_3D,ChanVsbarNpe_P_TBBigHit_3D)
+    analysis(cutflow3_D_dict,fileName2,fileName2,ChanVsbarNpe_TBBigHit_3D,ChanVsbarNpe_P_TBBigHit_3D)
+    analysis(cutflow3_D_dict,fileName3,fileName3,ChanVsbarNpe_TBBigHit_3D,ChanVsbarNpe_P_TBBigHit_3D)
+    analysis(cutflow3_A_dict,fileName1,fileName1,ChanVsbarNpe_TBBigHit_3A,ChanVsbarNpe_P_TBBigHit_3A)
+    analysis(cutflow3_A_dict,fileName2,fileName2,ChanVsbarNpe_TBBigHit_3A,ChanVsbarNpe_P_TBBigHit_3A)
+    analysis(cutflow3_A_dict,fileName3,fileName3,ChanVsbarNpe_TBBigHit_3A,ChanVsbarNpe_P_TBBigHit_3A)
+    analysis(cutflow3_B_dict,fileName1,fileName1,ChanVsbarNpe_TBBigHit_3B,ChanVsbarNpe_P_TBBigHit_3B)
+    analysis(cutflow3_B_dict,fileName2,fileName2,ChanVsbarNpe_TBBigHit_3B,ChanVsbarNpe_P_TBBigHit_3B)
+    analysis(cutflow3_B_dict,fileName3,fileName3,ChanVsbarNpe_TBBigHit_3B,ChanVsbarNpe_P_TBBigHit_3B)
+
+
+
+
+
+
+
 

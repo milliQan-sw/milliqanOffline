8e06f5b1acdb98ef63a683b2ab1b9c4833f04019
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-04-12 14:03:08 -0400

    Update 1D01histTimeDiff.py

--- Run3Detector/analysis/muonAna/1D01histTimeDiff.py
+++ Run3Detector/analysis/muonAna/1D01histTimeDiff.py
@@ -84,7 +84,7 @@ myiterator = milliqanProcessor(filelist, branches, myschedule, mycuts, myplotter
 myiterator.run()
 
 #create a new TFile
-f = r.TFile("1DhistTimeDiff0-1.root", "recreate")
+f = r.TFile("1dHistTimeDiff0-1.root", "recreate")
 
 #write the histograms to the file
 h_1d.Write()

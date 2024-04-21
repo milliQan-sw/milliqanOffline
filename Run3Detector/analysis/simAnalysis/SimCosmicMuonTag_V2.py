46628c5e9f0b99071772b317950bd65a911e0511
Collin Zheng <61294685+CollinKa@users.noreply.github.com>
2024-04-21 03:53:17 -0400

    Update SimCosmicMuonTag_V2.py

--- Run3Detector/analysis/simAnalysis/SimCosmicMuonTag_V2.py
+++ Run3Detector/analysis/simAnalysis/SimCosmicMuonTag_V2.py
@@ -291,7 +291,8 @@ def findCorrectTime(self,cutName = "DT_CorrectTime",cut = None,timeData = "time"
     diff1 = [[x] for x in diff1]
     diff2 = [[x] for x in diff2]
     TimeDiff = np.concatenate((diff1,diff2),axis = 1)
-    print(ak.to_list(ak.drop_none(TimeDiff,axis = 1)))
+    print(ak.to_list(ak.fill_none(TimeDiff, -50000))) #the none value will be overwirte with with -50000
+    #TimeDiff=ak.fill_none(TimeDiff, -50000)
     #self.events["maxTimeDTL0L3"] = ak.max(TimeDiff) #FIXME: can't find the max with none
      
 

771b53cdc823e2dd998b3a5eb6de7c579f4fc561
Ryan De Los Santos <ryansantos1174@gmail.com>
2024-04-22 11:54:44 -0400

    Update

--- Run3Detector/analysis/pulseInjection/GAN/gan.py
+++ Run3Detector/analysis/pulseInjection/GAN/gan.py
@@ -1,84 +1,74 @@
-from typing import Dict
-from tensorflow.keras.models import Sequential
-import matplotlib.pyplot as plt
-from tensorflow.keras.layers import Dense
-import numpy as np
-import numpy.typing as npt
+import tensorflow as tf
+from tensorflow.keras.layers import (Input, Dense, LeakyReLU, Embedding, Flatten,
+                                     Concatenate, Reshape, Activation)
+from tensorflow.keras.models import Model
 
+def build_generator(latent_dim, output_shape, embed_dim, num_classes):
+    noise = Input((latent_dim), name="noise_input")
+    x = Dense(256, name="gen_dense0")(noise)
+    x = LeakyReLU(0.2, name="gen_relu0")(x)
 
-class GAN():
-    def define_discriminator(self, n_inputs=100, optimizer='adam'):
-        model = Sequential()
-        model.add(Dense(25, activation='relu', kernel_initializer='he_uniform', input_dim=n_inputs))
-        model.add(Dense(1, activation='sigmoid'))
-        model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-        return model
+    label = Input((1), name="label")
+    l = Embedding(num_classes, embed_dim, input_length=1)(label)
+    l = Flatten()(l)
 
-    def define_generator(self, latent_dim, n_outputs=100):
-        model = Sequential()
-        model.add(Dense(500, activation = 'relu', kernel_initializer='he_uniform', input_dim=latent_dim))
-        model.add(Dense(n_outputs, activation='linear'))
-        return model
+    x = Concatenate()([x, l])
+    x = Dense(256, name="gen_dense1")(x)
+    x = LeakyReLU(0.2, name="gen_relu1")(x)
+    # x = dense(output_shape)(x)
+    # x = leakyrelu(0.2)(x)
 
-    def define_gan(self, generator, discriminator):
-        discriminator.trainable = False
-        model = Sequential()
-        model.add(generator)
-        model.add(discriminator)
-        # This will compile the generator as well
-        model.compile(loss='binary_crossentropy', optimizer='adam')
-        return model
+    output = Activation("tanh")(x)
+    output = Dense(output_shape, name="gen_dense2")(x)
+    return Model([noise, label], output, name="generator")
 
-    def generate_real_samples(self, isolated_waveforms, number_of_samples: int):
-        random_choices = np.random.randint(0, high=len(isolated_waveforms),
-                                        size=number_of_samples)
-        x_train = isolated_waveforms[random_choices]
-        print(len(isolated_waveforms[0]))
-        x_train = x_train.reshape(number_of_samples, len(isolated_waveforms[0]))
-        y = np.ones((number_of_samples, 1))
-        return x_train, y
+def build_discriminator(embed_dim, input_shape, num_classes):
+    waveform = Input((input_shape), name="discriminator_input")
+    x = Dense(64)(waveform)
+    x = LeakyReLU(0.2)(x)
 
-    def generate_latent_points(self, latent_dim, n):
-        x_input = np.random.randn(latent_dim, n)
-        x_input = x_input.reshape(n, latent_dim)
-        return x_input
+    label = Input((1), name="class_label")
+    l = Embedding(num_classes, embed_dim)(label)
+    l = Flatten()(l)
 
-    def generate_fake_samples(self, generator, latent_dim, n):
-        x_input = self.generate_latent_points(latent_dim, n)
-        gen_x = generator.predict(x_input)
-        gen_y = np.zeros((n, 1))
-        return gen_x, gen_y
+    x = Concatenate()([x, l])
+    output = Dense(1)(x)
 
-    def summarize_performance(self, epoch, generator, discriminator, latent_dim, isolated_waveforms, n=100):
+    return Model([waveform, label], output, name="discriminator")
 
-        x_real, y_real = self.generate_real_samples(isolated_waveforms, n)
-        _, acc_real = discriminator.evaluate(x_real, y_real, verbose=0)
-        x_fake, y_fake = self.generate_fake_samples(generator, latent_dim, n)
-        _, acc_fake = discriminator.evaluate(x_fake, y_fake, verbose=0)
-        print(epoch, acc_real, acc_fake)
-        plt.plot(x_real[0], color='red')
-        plt.plot(x_fake[0], color='blue')
-        plt.show()
+# The discriminator loss is defined as the combination of how well it is able
+# discriminate against real and fake data. The generator loss measures
+# how often the generated data is caught by the discriminator. Even ideally,
+# the loss should not go to 0, it will meet in the middle somewhere
+def train_step (real_waveforms, real_labels, latent_dim, generator, discriminator, g_opt, d_opt):
+    batch_size = tf.shape(real_waveforms)[0]
+    bce_loss = tf.keras.losses.BinaryCrossentropy(from_logits=True, label_smoothing=0.1)
 
-    def train(self, generator, discriminator,
-              gan, latent_dim, input_data: Dict[str, npt.NDArray], n_epochs=10000,
-            n_batch=32, n_eval=2000) -> list[float]:
+    noise = tf.random.normal([batch_size, latent_dim])
 
-        half_batch = int(n_batch/2)
-        loss = []
-        for i in range(n_epochs):
-            x_real, y_real = self.generate_real_samples(input_data, half_batch)
-            x_fake, y_fake = self.generate_fake_samples(generator, latent_dim, half_batch)
-            discriminator.train_on_batch(x_real, y_real)
-            discriminator.train_on_batch(x_fake, y_fake)
-            x_gan = self.generate_latent_points(latent_dim, n_batch)
+    for _ in range(3):
+        # Gradient tape keeps track of the forward pass so that you can back-propagate the errors
+        with tf.GradientTape() as dtape:
+            # Generate waveforms
+            generated_waveforms = generator([noise, real_labels], training=True)
 
-            # You only train the discriminator during GAN training
-            # Therefore, you want the generator to output data that passes as
-            # real data
+            # Train the discriminator over real data and generated data
+            real_output = discriminator([real_waveforms, real_labels], training=True)
+            fake_output = discriminator([generated_waveforms, real_labels], training=True)
 
-            y_gan = np.ones((n_batch, 1)) 
-            loss.append(gan.train_on_batch(x_gan, y_gan))
-            if (i + 1) % n_eval == 0:
-                self.summarize_performance(i, generator, discriminator, latent_dim, input_data)
-        return loss
+            # The loss of the GAN is the cumulative loss on the real and fake data
+            d_real_loss = bce_loss(tf.ones_like(real_output), real_output)
+            d_fake_loss = bce_loss(tf.zeros_like(fake_output), fake_output)
+            d_loss = (d_real_loss + d_fake_loss)/2
+
+        d_grad = dtape.gradient(d_loss, discriminator.trainable_variables)
+        d_opt.apply_gradients(zip(d_grad, discriminator.trainable_variables))
+
+    with tf.GradientTape() as gtape:
+        generated_waveforms = generator([noise, real_labels], training=True)
+        fake_output = discriminator([generated_waveforms, real_labels], training=True)
+        g_loss = bce_loss(tf.ones_like(fake_output), fake_output)
+    g_grad = gtape.gradient(g_loss, generator.trainable_variables)
+    g_opt.apply_gradients(zip(g_grad, generator.trainable_variables))
+
+    return g_opt, d_opt, d_loss, g_loss
--- Run3Detector/analysis/pulseInjection/GAN/main.py
+++ Run3Detector/analysis/pulseInjection/GAN/main.py
@@ -1,14 +1,16 @@
 import time
+import sys
 
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-import ganv2
-from preprocessing import WaveformProcessor
-from utilities import plot_loss
 
-tf.compat.v1.disable_eager_execution()
+import gan
+from preprocessing import WaveformProcessor
+import utilities
+# from utilities import plot_loss
+
 # Data Preprocessing Constants
 WAVEFORM_BOUNDS = (1200, 1600)
 SPE_AREA = 500
@@ -17,13 +19,15 @@ NS_PER_MEASUREMENT = 2.5
 # Model Constants
 LATENT_DIM = 500
 BATCH_SIZE = 128
-EPOCHS = 1000
-EVAL_EPOCH = 5000  # How often you should get output during training
+EPOCHS = 10000
+EVAL_EPOCH = 2000  # How often you should get output during training
+NUM_CLASSES = 3
 
 PLOT = False
 
 # Preprocess Waveform Data
-INPUT_FILE = "/home/ryan/Documents/Data/MilliQan/outputWaveforms_812_2p5V.root"
+INPUT_FILE = ("/home/ryan/Documents/Research/Data/MilliQanWaveforms/"
+              "outputWaveforms_812_2p5V.root")
 processor = WaveformProcessor(INPUT_FILE)
 
 
@@ -34,17 +38,28 @@ static_bounds = {key: np.array([[np.where(processor.times == WAVEFORM_BOUNDS[0])
 
 # Isolate Waveforms
 isolated_peaks = processor.isolate_waveforms(static_bounds)
+peak_heights = np.max(isolated_peaks, axis=1)
+utilities.plot_histogram(peak_heights, 100, 0, 300,
+                         write_to_existing_file=True,
+                         file_path="height.root")
+one_npe_fit = TF1("g1", "gaus", 
+
+# Create Height Classification
+
 
 npe = np.round(np.divide(np.trapz(isolated_peaks), SPE_AREA))
-num_classes = 12
-#num_classes = len(tf.unique(npe)[0])
+
+NUM_CLASSES = 3
+# num_classes = len(tf.unique(npe)[0])
 
 dataset = tf.data.Dataset.from_tensor_slices((isolated_peaks,
                                               npe))
 
-dataset = dataset.filter(lambda data, label: tf.logical_and(label >=0, label <12))
+dataset = dataset.filter(lambda data, label: tf.logical_and(label >= 0,
+                                                            label <= NUM_CLASSES))
 dataset = dataset.shuffle(buffer_size=200)
-dataset = dataset.batch(BATCH_SIZE).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+dataset = dataset.batch(BATCH_SIZE).prefetch(buffer_size=tf.data.experimental.
+                                             AUTOTUNE)
 
 if PLOT:
     for i, value in enumerate(isolated_peaks):
@@ -56,16 +71,16 @@ if PLOT:
         plt.savefig(f"Plots/isolated_peak_{i}.png")
 
 # # Defining GAN
-discriminator = ganv2.build_discriminator(embed_dim=128, input_shape=201,
-                                          num_classes=num_classes)
+discriminator = gan.build_discriminator(embed_dim=128, input_shape=201,
+                                          num_classes=NUM_CLASSES+1)
 
-generator = ganv2.build_generator(LATENT_DIM, output_shape=201, embed_dim=16,
-                                  num_classes=num_classes)
+generator = gan.build_generator(LATENT_DIM, output_shape=201, embed_dim=16,
+                                  num_classes=NUM_CLASSES+1)
 
 print(generator.summary())
 # Train Models
-generator_opt = tf.keras.optimizers.Adam()
-disc_opt = tf.keras.optimizers.Adam()
+generator_opt = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+disc_opt = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
 
 d_loss_values = []
 g_loss_values = [] 
@@ -80,8 +95,8 @@ for epoch in range(EPOCHS):
     i = 0
     for waveform_batch, label_batch in dataset:
         i+=1
-        d_batch_loss, g_batch_loss = ganv2.train_step(waveform_batch, label_batch,
-                                                      LATENT_DIM, num_classes,
+        generator_opt, disc_opt, d_batch_loss, g_batch_loss = gan.train_step(waveform_batch, label_batch,
+                                                      LATENT_DIM,
                                                       generator, discriminator,
                                                       generator_opt, disc_opt)
         d_loss += d_batch_loss
@@ -93,8 +108,8 @@ for epoch in range(EPOCHS):
     g_loss_values.append(g_loss)
 end = time.time()
 
-plot_loss(d_loss_values, g_loss_values, save_location="Plots/loss.png")
+print(f"Trained for {end-start} seconds!")
 
-print(f"Training took {end - start} seconds to complete.")
-discriminator.save("TrainedModels/discriminator.keras")
-generator.save("TrainedModels/generator.keras")
+plt.plot(d_loss_values)
+plt.plot(g_loss_values)
+plt.show()

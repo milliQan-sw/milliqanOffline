69acde5d7c91a028cdd09debc02480d2fbd882d9
Collin Zheng <61294685+CollinKa@users.noreply.github.com>
2024-04-23 13:44:08 -0400

    Update SimCosmicMuonTag_V2.py

--- Run3Detector/analysis/simAnalysis/SimCosmicMuonTag_V2.py
+++ Run3Detector/analysis/simAnalysis/SimCosmicMuonTag_V2.py
@@ -786,7 +786,7 @@ if __name__ == "__main__":
     myplotter.addHistograms(M_adj_NPE_C	, 'nPE', 'clean_Muon_adj_layer')
     NuniqueBar_C = r.TH1F("NuniqueBar_C" , "NuniqueBar;number of unique bar;events",50,0,50)
     myplotter.addHistograms(NuniqueBar_C, 'NBarsHits', 'Clean_MuonEvent')
-    CorrectTime_C =  r.TH1F("CorrectTime_C" , "D_t Max with correction w;D_t Max; Events",5000,0,5000)
+    CorrectTime_C =  r.TH1F("CorrectTime_C" , "D_t Max with correction w;D_t Max; Events",6000,-3000,3000)
     myplotter.addHistograms(CorrectTime_C, 'maxTimeDTL0L3', 'Clean_MuonEvent')
     NPERatio_C = r.TH1F("NPERatio_C","NPE ratio;max NPE/min NPE;Events",5000,0,5000)
     myplotter.addHistograms(NPERatio_C, 'BarNPERatio', 'Clean_MuonEvent')

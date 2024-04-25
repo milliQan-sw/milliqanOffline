ac66626c73a6b9d34188e668b320cdcaed40b709
Collin Zheng <61294685+CollinKa@users.noreply.github.com>
2024-04-25 18:32:21 -0400

    Create findTriggTime.py

---
+++ Run3Detector/analysis/simAnalysis/findTriggTime.py
@@ -0,0 +1,43 @@
+"""
+the goal of this file is to find the trigger time with data from timeFit_module_calibrated
+
+"""
+import math
+import os
+import sys
+import time
+import json
+
+sys.path.append("../utilities")
+
+from milliqanProcessor import *
+from milliqanScheduler import *
+from milliqanCuts import *
+from milliqanPlotter import *
+import awkward as ak
+
+
+branches = ["timeFit_module_calibrated","type"]
+
+
+numRun = str(sys.argv[1])
+fileNum = str(sys.argv[2])
+filelist =[f'/home/czheng/SimCosmicFlatTree/offlinefile/MilliQan_Run{numRun}.{fileNum}_v34.root:t']
+print(filelist)
+
+outputPath = str(sys.argv[3]) # the path is used at the very end for the output txt file
+print(outputPath)
+
+mycuts = milliqanCuts()
+
+myplotter = milliqanPlotter()
+
+
+T_h = r.TH1F("M_NPE", "nPE muon event layer", 250, 0, 2500)
+myplotter.addHistograms(T_h, 'timeFit_module_calibrated', 'barCut')
+
+cutflow = [mycuts.barCut,myplotter.dict['T_h']]
+
+f_out = r.TFile(f"{outputPath}/Run{numRun}_file{fileNum}_findTrigger.root", "RECREATE")
+T_h.Write()
+f_out.Close()

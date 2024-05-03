701f8c34607b85d2e48485f5ba65f839e1006a3f
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-05-03 14:46:29 -0400

    Update 1DhistTimeDiff.py

--- Run3Detector/analysis/muonAna/1DhistTimeDiff.py
+++ Run3Detector/analysis/muonAna/1DhistTimeDiff.py
@@ -63,7 +63,7 @@ def getTimeDiff(self):
 
     # print out channel number and corresponding desired times of events in that channel
     for key, value in cor_times.items():
-        if ak.is_none(value): 
+        if ak.num(value) == 0: 
             print(key, [None, None])
         else:
             print(key, value)

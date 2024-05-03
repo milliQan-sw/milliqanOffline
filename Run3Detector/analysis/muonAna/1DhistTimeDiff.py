64eff65a596dbbe054f07a43bb2a45877bfbb9e0
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-05-03 15:40:24 -0400

    Update 1DhistTimeDiff.py

--- Run3Detector/analysis/muonAna/1DhistTimeDiff.py
+++ Run3Detector/analysis/muonAna/1DhistTimeDiff.py
@@ -65,6 +65,7 @@ def getTimeDiff(self):
 
     # create an empty awkward array to store time differences initialized with all Nones
     time_diffs = ak.Array([None] * 1000)
+    print(time_diffs[2])
 
     # calculate time differences between layer 1 and layer 0 in each channel for each event
     for row in range(4):

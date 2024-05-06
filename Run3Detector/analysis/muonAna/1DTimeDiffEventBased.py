82168a74b0a0dbd3ef5c46d08dc76d42d5f12117
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-05-06 15:16:08 -0400

    Update 1DTimeDiffEventBased.py

--- Run3Detector/analysis/muonAna/1DTimeDiffEventBased.py
+++ Run3Detector/analysis/muonAna/1DTimeDiffEventBased.py
@@ -17,8 +17,76 @@ from milliqanPlotter import *
 
 # define the function to get the time differences between events in each channel between layer 0 and layer 1
 def getTimeDiff(self):
-    
+    # branches used for locating events in different channels
+    rows = self.events['row'][self.events['straightLineCut']]
+    columns = self.events['column'][self.events['straightLineCut']]
+    layers = self.events['layer'][self.events['straightLineCut']]
+    # branches with data to extract from
+    heights = self.events['height'][self.events['straightLineCut']]
+    times = self.events['timeFit_module_calibrated'][self.events['straightLineCut']]
 
+    # initialize dictionaries to hold max pulse heights and corresponding times
+    max_heights = {}
+    cor_times = {}
+
+    # iterate over each channel (using row/column/layer to locate each channel)
+    for row in range(4):
+        for column in range(4):
+            for layer in range(2):
+                # generate key for dictionaries
+                key = (row, column, layer)
+
+                # define the location mask for the current channel
+                locationMask = (rows == row) & (columns == column) & (layers == layer)
+
+                # get heights and times at current channel (they should have the exact same dimension)
+                channel_heights = heights[locationMask]
+                channel_times = times[locationMask]
+
+                # find the max height of each event (sublist) and its corresponding time
+                if ak.any(ak.num(channel_heights) > 0):  # check if any sublist in channel_heights is not empty in case it's an empty array
+                    # store the list of max heights in each event (sublist) into the dictionary
+                    max_heights[key] = ak.max(channel_heights, axis=-1) # now channel_heights is flattened
+
+                    # boolean mask to know which pulse (element) in each event (sublist) achieves its max height
+                    max_mask = (channel_heights == ak.broadcast_arrays(max_heights[key], channel_heights)[0])
+
+                    # use the mask to pick out the corresponding times to the max pulse heights (element) in each event (sublist)
+                    raw_cor_times = ak.mask(channel_times, max_mask)# an unflattened raw 2D array with only desired times
+
+                    # for each event, extract the corresponding time or return None if there's no pulse in that event
+                    cor_times[key] = ak.Array([
+                        next((item for item in sublist if item is not None), None) 
+                        if sublist is not None else None  # check if sublist is None
+                        for sublist in ak.to_list(raw_cor_times)
+                    ])
+                    # print out channel number and corresponding desired times of events in that channel
+                    print(key, cor_times[key])
+
+    # create an empty list to store time differences initialized with all Nones
+    time_diffs = []
+    # calculate time differences between layer 1 and layer 0 in each channel for each event
+    for row in range(4):
+        for column in range(4):
+            # define keys to locate time
+            key0 = (row, column, 0)
+            key1 = (row, column, 1)
+
+            if key0 in cor_times and key1 in cor_times:
+                for i in range(len(times)): # iterate over each event that has passed straightLineCut
+                    if cor_times[key0][i] is not None and cor_times[key1][i] is not None:
+                        time_diff = cor_times[key1][i] - cor_times[key0][i]
+                        time_diffs.append(time_diff)
+    
+    print(time_diffs)
+    print("Effective time differences total number: ", len(time_diffs))
+
+    # extend time_diffs to a 1000 sized list with Nones
+    num_nones = 1000 - len(time_diffs)
+    time_diffs.extend([None] * num_nones)
+
+    # store the time differences in the 'timeDiff' branch
+    self.events['timeDiff'] = time_diffs
 
 # add our custom function to milliqanCuts
 setattr(milliqanCuts, 'getTimeDiff', getTimeDiff)

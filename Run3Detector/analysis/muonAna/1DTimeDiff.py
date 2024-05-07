af1e9ab149b685fe344bed2ebae33f1c3d6f8b22
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-05-07 16:17:48 -0400

    Update 1DTimeDiff.py

--- Run3Detector/analysis/muonAna/1DTimeDiff.py
+++ Run3Detector/analysis/muonAna/1DTimeDiff.py
@@ -79,7 +79,62 @@ def getTimeDiff(self):
 
     self.events['timeDiff'] = time_diffs
 
-    '''
+# add our custom function to milliqanCuts
+setattr(milliqanCuts, 'getTimeDiff', getTimeDiff)
+
+# define a file list to run over
+filelist = ['/mnt/hadoop/se/store/user/milliqan/trees/v34/1000/MilliQan_Run1006.4_v34.root:t']
+
+# define the necessary branches to run over
+branches = ['pickupFlag', 'boardsMatched', 'timeFit_module_calibrated', 'height', 'area', 'column', 'row', 'layer', 'chan', 'ipulse', 'type']
+
+# define the milliqan cuts object
+mycuts = milliqanCuts()
+
+# require pulses are not pickup
+pickupCut = mycuts.getCut(mycuts.pickupCut, 'pickupCut', cut=True, branches=branches)
+
+# require that all digitizer boards are matched
+boardMatchCut = mycuts.getCut(mycuts.boardsMatched, 'boardMatchCut', cut=True, branches=branches)
+
+# add four layer cut
+fourLayerCut = mycuts.getCut(mycuts.fourLayerCut, 'fourLayerCut', cut=False)
+
+# define milliqan plotter
+myplotter = milliqanPlotter()
+
+# create a 1D root histogram
+h_1d = r.TH1F("h_1d", "timeFit_module_calibrated Differences between layer 0 and 1", 100, -50, 50)
+h_1d.GetXaxis().SetTitle("time difference between layer 0 and 1")
+
+# add root histogram to plotter
+myplotter.addHistograms(h_1d, 'timeDiff')
+
+# defining the cutflow
+cutflow = [boardMatchCut, pickupCut, mycuts.layerCut, mycuts.straightLineCut, mycuts.getTimeDiff, myplotter.dict['h_1d']]
+
+# create a schedule of the cuts
+myschedule = milliQanScheduler(cutflow, mycuts, myplotter)
+
+# print out the schedule
+myschedule.printSchedule()
+
+# create the milliqan processor object
+myiterator = milliqanProcessor(filelist, branches, myschedule, mycuts, myplotter)
+
+# run the milliqan processor
+myiterator.run()
+
+# create a new TFile
+f = r.TFile("1DhistTimeDiff.root", "recreate")
+
+# write the histograms to the file
+h_1d.Write()
+
+# close the file
+f.Close()
+
+'''
     # iterate over each channel (using row/column/layer to locate each channel)
     for row in range(4):
         for column in range(4):
@@ -138,59 +193,4 @@ def getTimeDiff(self):
 
     # store the time differences in the 'timeDiff' branch
     self.events['timeDiff'] = time_diffs
-    '''
-
-# add our custom function to milliqanCuts
-setattr(milliqanCuts, 'getTimeDiff', getTimeDiff)
-
-# define a file list to run over
-filelist = ['/mnt/hadoop/se/store/user/milliqan/trees/v34/1000/MilliQan_Run1006.4_v34.root:t']
-
-# define the necessary branches to run over
-branches = ['pickupFlag', 'boardsMatched', 'timeFit_module_calibrated', 'height', 'area', 'column', 'row', 'layer', 'chan', 'ipulse', 'type']
-
-# define the milliqan cuts object
-mycuts = milliqanCuts()
-
-# require pulses are not pickup
-pickupCut = mycuts.getCut(mycuts.pickupCut, 'pickupCut', cut=True, branches=branches)
-
-# require that all digitizer boards are matched
-boardMatchCut = mycuts.getCut(mycuts.boardsMatched, 'boardMatchCut', cut=True, branches=branches)
-
-# add four layer cut
-fourLayerCut = mycuts.getCut(mycuts.fourLayerCut, 'fourLayerCut', cut=False)
-
-# define milliqan plotter
-myplotter = milliqanPlotter()
-
-# create a 1D root histogram
-h_1d = r.TH1F("h_1d", "timeFit_module_calibrated Differences between layer 0 and 1", 100, -50, 50)
-h_1d.GetXaxis().SetTitle("time difference between layer 0 and 1")
-
-# add root histogram to plotter
-myplotter.addHistograms(h_1d, 'timeDiff')
-
-# defining the cutflow
-cutflow = [boardMatchCut, pickupCut, mycuts.layerCut, mycuts.straightLineCut, mycuts.getTimeDiff, myplotter.dict['h_1d']]
-
-# create a schedule of the cuts
-myschedule = milliQanScheduler(cutflow, mycuts, myplotter)
-
-# print out the schedule
-myschedule.printSchedule()
-
-# create the milliqan processor object
-myiterator = milliqanProcessor(filelist, branches, myschedule, mycuts, myplotter)
-
-# run the milliqan processor
-myiterator.run()
-
-# create a new TFile
-f = r.TFile("1DhistTimeDiff.root", "recreate")
-
-# write the histograms to the file
-h_1d.Write()
-
-# close the file
-f.Close()
+'''

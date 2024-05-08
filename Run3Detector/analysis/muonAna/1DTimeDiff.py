55ef56aa93a92e7ea59d367ac2f155af7027217f
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-05-08 15:20:03 -0400

    Update 1DTimeDiff.py

--- Run3Detector/analysis/muonAna/1DTimeDiff.py
+++ Run3Detector/analysis/muonAna/1DTimeDiff.py
@@ -98,19 +98,6 @@ filelist = [
     '/mnt/hadoop/se/store/user/milliqan/run3/bar/1100/0007/MilliQan_Run1178.422_default.root:Events'
 ]
 
-'''
-Check branches in files in root:
-
-TFile* file = TFile::Open("/mnt/hadoop/se/store/user/milliqan/trees/v34/1000/MilliQan_Run1006.1_v34.root");
-TTree* tree = (TTree*)file->Get("t");
-tree->Print();
-
-TFile* file = TFile::Open("/mnt/hadoop/se/store/user/milliqan/run3/bar/1100/0007/MilliQan_Run1178.422_default.root");
-TTree* tree = (TTree*)file->Get("Events");
-tree->Print();
-
-'''
-
 # define the necessary branches to run over
 branches = ['pickupFlag', 'boardsMatched', 'timeFit_module_calibrated', 'height', 'area', 'column', 'row', 'layer', 'chan', 'ipulse', 'type']
 
@@ -159,6 +146,19 @@ h_1d.Write()
 
 # close the file
 f.Close()
+
+'''
+Check branches in files in root:
+
+TFile* file = TFile::Open("/mnt/hadoop/se/store/user/milliqan/trees/v34/1000/MilliQan_Run1006.1_v34.root");
+TTree* tree = (TTree*)file->Get("t");
+tree->Print();
+
+TFile* file = TFile::Open("/mnt/hadoop/se/store/user/milliqan/run3/bar/1100/0007/MilliQan_Run1178.422_default.root");
+TTree* tree = (TTree*)file->Get("Events");
+tree->Print();
+
+'''
 
 
 '''

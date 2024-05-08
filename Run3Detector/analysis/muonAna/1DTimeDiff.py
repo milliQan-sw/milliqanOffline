cb258beff3470e7e7206b581fea419e3377bb1a5
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-05-08 13:48:20 -0400

    Update 1DTimeDiff.py

--- Run3Detector/analysis/muonAna/1DTimeDiff.py
+++ Run3Detector/analysis/muonAna/1DTimeDiff.py
@@ -95,12 +95,7 @@ setattr(milliqanCuts, 'getTimeDiff', getTimeDiff)
 
 # define a file list to run over
 filelist = [
-    '/store\/user\/milliqan\/run3\/bar\/\/1100\/0007","MilliQan_Run1178.422_default.root',
-    '/store\/user\/milliqan\/run3\/bar\/\/1100\/0007","MilliQan_Run1178.423_default.root',
-    '/store\/user\/milliqan\/run3\/bar\/\/1100\/0007","MilliQan_Run1178.424_default.root',
-    '/store\/user\/milliqan\/run3\/bar\/\/1100\/0007","MilliQan_Run1178.425_default.root',
-    '/store\/user\/milliqan\/run3\/bar\/\/1100\/0007","MilliQan_Run1178.426_default.root',
-    '/store\/user\/milliqan\/run3\/bar\/\/1100\/0007","MilliQan_Run1178.427_default.root'
+    '/store/user/milliqan/run3/bar/1100/0007","MilliQan_Run1178.422_default.root'
 ]
 
 # define the necessary branches to run over

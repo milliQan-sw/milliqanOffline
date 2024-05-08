ec80f55b2efaa80a55f0b8d11a044681393189c6
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-05-08 13:16:58 -0400

    Update 1DTimeDiff.py

--- Run3Detector/analysis/muonAna/1DTimeDiff.py
+++ Run3Detector/analysis/muonAna/1DTimeDiff.py
@@ -36,10 +36,11 @@ def getTimeDiff(self):
 
             # 1D True/False mask determined by whether there is any straight line pass 
             event_mask = ak.any(pulse_maskL0, axis=1) & ak.any(pulse_maskL1, axis=1) & ak.any(pulse_maskL2, axis=1) & ak.any(pulse_maskL3, axis=1)
+            height_mask = self.events['height'][self.events['height'] > 1000]
 
             # select events on straight line passes then select layers
-            mask0 = event_mask & pulse_maskL0
-            mask1 = event_mask & pulse_maskL1
+            mask0 = event_mask & pulse_maskL0 & height_mask
+            mask1 = event_mask & pulse_maskL1 & height_mask
 
             heightsL0 = self.events['height'][mask0]  # 2D heights in one channel on layer 0
             timeL0 = self.events['timeFit_module_calibrated'][mask0]  # 2D times in one channel on layer 0
@@ -78,7 +79,7 @@ def getTimeDiff(self):
 
     for key in max_heightsL1:
         print(key, "layer 1 Max Height: ", ak.max(max_heightsL1[key]))
-        
+
     print()
 
     print(time_diffs)

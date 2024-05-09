553dfd3c5d761e274884c2ed6fa910bb0125cc97
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-05-09 11:52:21 -0400

    Update 1DTimeDiff.py

--- Run3Detector/analysis/muonAna/1DTimeDiff.py
+++ Run3Detector/analysis/muonAna/1DTimeDiff.py
@@ -95,7 +95,16 @@ setattr(milliqanCuts, 'getTimeDiff', getTimeDiff)
 
 # define a file list to run over
 filelist = [
-    '/mnt/hadoop/se/store/user/milliqan/trees/v34/1100/MilliQan_Run1178.528_v34.root'
+    '/mnt/hadoop/se/store/user/milliqan/trees/v34/1100/MilliQan_Run1178.520_v34.root',
+    '/mnt/hadoop/se/store/user/milliqan/trees/v34/1100/MilliQan_Run1178.521_v34.root',
+    '/mnt/hadoop/se/store/user/milliqan/trees/v34/1100/MilliQan_Run1178.522_v34.root',
+    '/mnt/hadoop/se/store/user/milliqan/trees/v34/1100/MilliQan_Run1178.523_v34.root',
+    '/mnt/hadoop/se/store/user/milliqan/trees/v34/1100/MilliQan_Run1178.524_v34.root',
+    '/mnt/hadoop/se/store/user/milliqan/trees/v34/1100/MilliQan_Run1178.525_v34.root',
+    '/mnt/hadoop/se/store/user/milliqan/trees/v34/1100/MilliQan_Run1178.526_v34.root',
+    '/mnt/hadoop/se/store/user/milliqan/trees/v34/1100/MilliQan_Run1178.527_v34.root',
+    '/mnt/hadoop/se/store/user/milliqan/trees/v34/1100/MilliQan_Run1178.528_v34.root',
+    '/mnt/hadoop/se/store/user/milliqan/trees/v34/1100/MilliQan_Run1178.529_v34.root'
 ]
 
 # define the necessary branches to run over

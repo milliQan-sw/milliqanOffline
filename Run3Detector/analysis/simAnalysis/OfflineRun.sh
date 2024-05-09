a6cf5d651768d3c0647e5dd43d8479c190ff5da6
Collin Zheng <zheng.1947@osu.edu>
2024-05-09 00:50:39 -0400

    MQ PC cutflow 7 offline analysis output data location

--- Run3Detector/analysis/simAnalysis/OfflineRun.sh
+++ Run3Detector/analysis/simAnalysis/OfflineRun.sh
@@ -2,5 +2,5 @@
 
 for ((num = 1; num <= 207; num ++))
 do
-    python3 OLCosmicMuonTag_V2.py 1163  $num /home/czheng/SimCosmicFlatTree/cutEffCheckResult_OffL_20NPEcut
+    python3 OLCosmicMuonTag_V2.py 1163  $num /home/czheng/SimCosmicFlatTree/cutEffCheckResult_OffL_20NPEcut/cosmicStraight/Histograms/cutflow7
 done    

f8c95dbea6d0478f73cba26fc5c55c311b24c4a7
Collin Zheng <zheng.1947@osu.edu>
2024-05-09 00:49:53 -0400

    MQ PC cutflow 7 sim analysis output data location

--- Run3Detector/analysis/simAnalysis/MQmachineRun.sh
+++ Run3Detector/analysis/simAnalysis/MQmachineRun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for ((num = 1; num <= 100; num ++))
+for ((num = 1; num <= 300; num ++))
 do
-    python3 SimCosmicMuonTag_V2.py $num /home/czheng/SimCosmicFlatTree/cutEffCheckResult_sim_20NPEcut/CosmicStraight/purestraightCut
+    python3 SimCosmicMuonTag_V2.py $num /home/czheng/SimCosmicFlatTree/cutEffCheckResult_sim_20NPEcut/CosmicStraight/purestraightCut/cutflow7
 done    

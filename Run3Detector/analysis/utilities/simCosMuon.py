a97078f070f9cc5e66601ff1a1b88bb411e1b094
Collin Zheng <61294685+CollinKa@users.noreply.github.com>
2024-05-09 20:25:45 -0400

    remove outdated comment

--- Run3Detector/analysis/utilities/simCosMuon.py
+++ Run3Detector/analysis/utilities/simCosMuon.py
@@ -83,7 +83,6 @@ myplotter.addHistograms(CorrectTime_default_OL, 'DTL0L3', 'StraghtCosmic')
 
 NPERatio_C = r.TH1F("NPERatio_C","NPE ratio;max NPE/min NPE;Events",5000,0,5000)
 myplotter.addHistograms(NPERatio_C, 'BarNPERatio', 'Clean_MuonEvent')
-#FIXME:mycuts.offlinePreProcess   this method cause the find correctTime crash 
 cutflow7 = [mycuts.EmptyListFilter,mycuts.countEvent,mycuts.barCut,mycuts.panelCut,mycuts.BarNPERatioCalculate,mycuts.NbarsHitsCount,mycuts.sudo_straight,NpeRatio_adj_tag,NpeRatio_ot_tag,clean_Muon_layer,clean_Muon_adj_layer,cleanMuon_count,clean_Muon_Dt,myplotter.dict['M_NPE_C'],myplotter.dict['M_adj_NPE_C'],myplotter.dict["NuniqueBar_C"],myplotter.dict["NPERatio_C"],myplotter.dict["CorrectTime_OL"],myplotter.dict["CorrectTime_default_OL"],myplotter.dict["NpeRatio_adj"],myplotter.dict["NpeRatio_ot"]]    
 
 

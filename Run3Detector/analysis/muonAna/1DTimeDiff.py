f6de7875222f08e64ddbbec5c6487b382ade9bd4
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-05-20 12:26:51 -0400

    Update 1DTimeDiff.py

--- Run3Detector/analysis/muonAna/1DTimeDiff.py
+++ Run3Detector/analysis/muonAna/1DTimeDiff.py
@@ -32,15 +32,16 @@ def getTimeDiff(self):
     
     time_diffsL30 = []
 
+# iterate over each straight line
     for row in range(4):
         for column in range(4):
-            # 2D True/False masks determined by channels (at current channel?)
+            # pulse_based 2D boolean masks determined by channel and height (at current channel? height > 1000?)
             pulse_maskL0 = (self.events['row'] == row) & (self.events['column'] == column) & (self.events['layer'] == 0) & (self.events['height'] > 1000)
             pulse_maskL1 = (self.events['row'] == row) & (self.events['column'] == column) & (self.events['layer'] == 1) & (self.events['height'] > 1000)
             pulse_maskL2 = (self.events['row'] == row) & (self.events['column'] == column) & (self.events['layer'] == 2) & (self.events['height'] > 1000)
             pulse_maskL3 = (self.events['row'] == row) & (self.events['column'] == column) & (self.events['layer'] == 3) & (self.events['height'] > 1000)
 
-            # 1D True/False mask determined by event (any straight line pass?)
+            # event_based 1D boolean mask determined by event (any straight line passes at current channel?)
             event_mask = ak.any(pulse_maskL0, axis=1) & ak.any(pulse_maskL1, axis=1) & ak.any(pulse_maskL2, axis=1) & ak.any(pulse_maskL3, axis=1)
 
             # select events on straight line passes then select layers

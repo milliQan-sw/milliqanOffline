8d6eba68508551cc86d2450f36faa40890c88ece
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-05-20 13:15:27 -0400

    Update 1DTimeDiff.py

--- Run3Detector/analysis/muonAna/1DTimeDiff.py
+++ Run3Detector/analysis/muonAna/1DTimeDiff.py
@@ -100,8 +100,10 @@ def getTimeDiff(self):
                 for sublist in ak.to_list(raw_max_timesL3)
             ])
 
-            if ak.max(max_timeL0[key]) is not None and ak.max(max_timeL1[key]) is not None and ak.max(max_timeL2[key]) is not None and ak.max(max_timeL3[key]) is not None:
-                time_diffsL30.append(ak.max(max_timeL3[key]) - ak.max(max_timeL0[key]))
+            # iterate over events
+            for event in max_timeL0[key]:
+                if max_timeL0[key][event] is not None and max_timeL1[key][event] is not None and max_timeL2[key][event] is not None and max_timeL3[key][event] is not None:
+                    time_diffsL30.append(max_timeL3[key][event] - max_timeL0[key][event])
 
     print(time_diffsL30)
 

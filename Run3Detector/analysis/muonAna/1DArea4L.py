470ec22498ed94dfa1857bf09a9d3ae8f6b5bc54
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-05-21 16:21:56 -0400

    update

--- Run3Detector/analysis/muonAna/1DArea4L.py
+++ Run3Detector/analysis/muonAna/1DArea4L.py
@@ -33,15 +33,15 @@ def getArea(self):
     areas = []
 
     # remove events with panel pulses that pass the height cut
-    panel_pulse_mask = (self.events['height'] > 500) & (self.events['type'] == 2)
+    panel_pulse_mask = (self.events['type'] == 2) & (self.events['height'] > 1000)
     events_without_panel_pulses = ~ak.any(panel_pulse_mask, axis = 1)
 
     # ensure that events have slab pulses that pass the height cut
-    slab_pulse_mask = (self.events['height'] > 0) & (self.events['type'] == 1)
+    slab_pulse_mask = (self.events['type'] == 1) & (self.events['height'] > 0)
     events_with_slab_pulses = ak.any(slab_pulse_mask, axis = 1)
 
     # combine masks to get valid events
-    valid_events_mask = events_without_panel_pulses & events_with_slab_pulses
+    valid_events_mask = events_without_panel_pulses #& events_with_slab_pulses
 
 # iterate over straight line passes
     for row in range(4):

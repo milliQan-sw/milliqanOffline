05ea80c37d4dd78dd8311512c540ad547dd00d96
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-05-21 15:55:54 -0400

    Update 1DTimeDiffL30.py

--- Run3Detector/analysis/muonAna/1DTimeDiffL30.py
+++ Run3Detector/analysis/muonAna/1DTimeDiffL30.py
@@ -33,7 +33,7 @@ def getTimeDiff(self):
     time_diffsL30 = []
 
     # remove events with panel pulses that pass the height cut
-    panel_pulse_mask = (self.events['height'] > 500) & (self.events['type'] == 2)
+    panel_pulse_mask = (self.events['height'] > 1000) & (self.events['type'] == 2)
     events_without_panel_pulses = ~ak.any(panel_pulse_mask, axis = 1)
 
     # ensure that events have slab pulses that pass the height cut

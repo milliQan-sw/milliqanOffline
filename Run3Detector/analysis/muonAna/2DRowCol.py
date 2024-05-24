f7134053b93a429222292884ee96b3276466f053
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-05-24 16:55:34 -0400

    Update 2DRowCol.py

--- Run3Detector/analysis/muonAna/2DRowCol.py
+++ Run3Detector/analysis/muonAna/2DRowCol.py
@@ -209,12 +209,3 @@ myiterator = milliqanProcessor(filelist, branches, myschedule, mycuts, myplotter
 
 # run the milliqan processor
 myiterator.run()
-
-# create a new TFile
-f = r.TFile("2DhistRowCol.root", "recreate")
-
-# write the histograms to the file
-h_2d.Write()
-
-# close the file
-f.Close()

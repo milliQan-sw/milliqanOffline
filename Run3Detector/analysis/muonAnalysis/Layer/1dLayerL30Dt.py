545c404fe8c6544034941d0c60891211adc40386
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-06-12 14:17:59 -0400

    Update 1dLayerL30Dt.py

--- Run3Detector/analysis/muonAnalysis/Layer/1dLayerL30Dt.py
+++ Run3Detector/analysis/muonAnalysis/Layer/1dLayerL30Dt.py
@@ -130,7 +130,7 @@ myiterator = milliqanProcessor(filelist, branches, myschedule, mycuts, myplotter
 myiterator.run()
 
 # create a new TFile
-f = r.TFile("S1500LayerL30Dt.root", "recreate")
+f = r.TFile("S1000LayerL30Dt.root", "recreate")
 
 # write the histograms to the file
 h_1d.Write()

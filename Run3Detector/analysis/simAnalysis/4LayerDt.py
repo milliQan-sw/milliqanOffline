8b12f473e4f436199657147c0859956886cecd79
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-06-18 11:43:12 -0400

    Update 4LayerDt.py

--- Run3Detector/analysis/simAnalysis/4LayerDt.py
+++ Run3Detector/analysis/simAnalysis/4LayerDt.py
@@ -24,7 +24,7 @@ def getTimeDiff(self):
     nPEMask = self.events['hit_nPE'] > 200
 
     # muon mask
-    muonMask = abs(self.events['hit_particleName']) != 13
+    muonMask = abs(self.events['hit_particleName']) == 13
 
     # cut off muons that do not hit the detector
     hitMask = self.events['hit_hitPositionZ_cm'] != 0

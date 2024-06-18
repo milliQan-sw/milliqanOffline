30a259ad7dbc54ffa96fadc4556e149db8a05063
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-06-18 12:01:04 -0400

    Update 3LayerDt.py

--- Run3Detector/analysis/simAnalysis/3LayerDt.py
+++ Run3Detector/analysis/simAnalysis/3LayerDt.py
@@ -57,7 +57,7 @@ def getTimeDiff(self):
         # require pulses in all 4 layers for one event
         if timeL0_min[i] is not None and timeL1_min[i] is not None and timeL3_min[i] is not None:
             # calculate time differences only for events with valid times in all layers
-            time_diffsL30.append(timeL3_min[i] - timeL1_min[i])
+            time_diffsL30.append(timeL1_min[i] - timeL0_min[i])
     
     print(time_diffsL30)
 
@@ -84,7 +84,7 @@ mycuts = milliqanCuts()
 myplotter = milliqanPlotter()
 
 # create a 1D root histogram
-h_1d = r.TH1F("h_1d", "Time Differences between Layer 3 and 1", 100, -50, 50)
+h_1d = r.TH1F("h_1d", "Time Differences between Layer 1 and 0", 100, -50, 50)
 h_1d.GetXaxis().SetTitle("Time Differences")
 
 # add root histogram to plotter
@@ -106,7 +106,7 @@ myiterator = milliqanProcessor(filelist, branches, myschedule, mycuts, myplotter
 myiterator.run()
 
 # create a new TFile
-f = r.TFile("3LayerDt2ExL31.root", "recreate")
+f = r.TFile("3LayerDt2ExL10.root", "recreate")
 
 # write the histograms to the file
 h_1d.Write()

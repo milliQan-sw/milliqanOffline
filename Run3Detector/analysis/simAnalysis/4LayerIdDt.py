f4647ac6923e130c55cf2e758c27819a4f0b4eb5
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-06-21 14:24:47 -0400

    Create 4LayerIdDt.py

---
+++ Run3Detector/analysis/simAnalysis/4LayerIdDt.py
@@ -0,0 +1,109 @@
+# importing packages
+import os
+import ROOT as r
+import uproot
+import hist
+import matplotlib.pyplot as plt
+import awkward as ak
+import numpy as np
+import pandas as pd
+import array as arr
+import sys
+sys.path.append('../utilities')
+from milliqanProcessor import *
+from milliqanScheduler import *
+from milliqanCuts import *
+from milliqanPlotter import *
+
+# define the function to get the time differences
+def getTimeDiff(self):
+    
+    time_diffsL30 = []
+
+    # nPE mask to replace height and area mask
+    nPEMask = self.events['nPE'] > 10000
+
+    # make final mask
+    finalPulseMask = nPEMask
+
+    # apply the finalPulseMask
+    masked_time = self.events['time'][finalPulseMask]
+    masked_layer = self.events['layer'][finalPulseMask]
+
+    # masked times per layer
+    timeL0 = masked_time[masked_layer == 0]
+    timeL1 = masked_time[masked_layer == 1]
+    timeL2 = masked_time[masked_layer == 2]
+    timeL3 = masked_time[masked_layer == 3]
+
+    # function to get minimum time per event handling None values
+    def minTime(pulse_times):
+        filtered_times = [time for time in pulse_times if time is not None]
+        return min(filtered_times) if filtered_times else None
+
+    # extract minimum times for each layer
+    timeL0_min = [minTime(event) for event in ak.to_list(timeL0)]
+    timeL1_min = [minTime(event) for event in ak.to_list(timeL1)]
+    timeL2_min = [minTime(event) for event in ak.to_list(timeL2)]
+    timeL3_min = [minTime(event) for event in ak.to_list(timeL3)]
+
+    for i in range(len(timeL0_min)):
+        # require pulses in all 4 layers for one event
+        if timeL0_min[i] is not None and timeL1_min[i] is not None and timeL2_min[i] is not None and timeL3_min[i] is not None:
+            # calculate time differences only for events with valid times in all layers
+            time_diffsL30.append(timeL3_min[i] - timeL0_min[i])
+    
+    print(time_diffsL30)
+
+    # extend the final list to match the size of the current file
+    num_events = len(self.events)
+    num_nones = num_events - len(time_diffsL30)
+    time_diffsL30.extend([None] * num_nones)
+
+    # define custom branch
+    self.events['timeDiff'] = time_diffsL30
+
+# add our custom function to milliqanCuts
+setattr(milliqanCuts, 'getTimeDiff', getTimeDiff)
+
+filelist = ['/home/bpeng/muonAnalysis/dy_nophoton_flat.root']
+
+# define the necessary branches to run over
+branches = ['hit_hitTime_ns', 'hit_nPE', 'hit_layer', 'hit_particleName', 'layer', 'nPE', 'time', 'chan']
+
+# define the milliqan cuts object
+mycuts = milliqanCuts()
+
+# define milliqan plotter
+myplotter = milliqanPlotter()
+
+# create a 1D root histogram
+h_1d = r.TH1F("h_1d", "Time Differences between Layer 3 and 0", 100, -50, 50)
+h_1d.GetXaxis().SetTitle("Time Differences")
+
+# add root histogram to plotter
+myplotter.addHistograms(h_1d, 'timeDiff')
+
+# defining the cutflow
+cutflow = [mycuts.getTimeDiff, myplotter.dict['h_1d']]
+
+# create a schedule of the cuts
+myschedule = milliQanScheduler(cutflow, mycuts, myplotter)
+
+# print out the schedule
+myschedule.printSchedule()
+
+# create the milliqan processor object
+myiterator = milliqanProcessor(filelist, branches, myschedule, mycuts, myplotter)
+
+# run the milliqan processor
+myiterator.run()
+
+# create a new TFile
+f = r.TFile("4LayerDtL30.root", "recreate")
+
+# write the histograms to the file
+h_1d.Write()
+
+# close the file
+f.Close()

0efbb6ce4fd809f4e100cbb6d400b9237197b7a9
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-06-21 23:24:26 -0400

    Update RunDtFit.py

--- Run3Detector/analysis/timingCorrection/RunDtFit.py
+++ Run3Detector/analysis/timingCorrection/RunDtFit.py
@@ -135,7 +135,7 @@ myiterator = milliqanProcessor(filelist, branches, myschedule, mycuts, myplotter
 myiterator.run()
 
 # create a new TFile
-f = r.TFile("Run10?0-10?9DtL30.root", "recreate")######################################################################################################
+f = r.TFile("Run10_0_10_9DtL30.root", "recreate")######################################################################################################
 
 # write the histograms to the file
 h_1d.Write()

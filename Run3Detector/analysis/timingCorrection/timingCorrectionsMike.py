4d7916010f9f5f3f8ec51e87cac56d4e328b321b
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-06-24 13:47:39 -0400

    Create timingCorrectionsMike.py

---
+++ Run3Detector/analysis/timingCorrection/timingCorrectionsMike.py
@@ -0,0 +1,65 @@
+import sys
+sys.path.append('/root/lib/')
+import os
+import ROOT as r
+import pandas as pd
+import json
+import shutil
+%jsroot on
+
+shutil.copy('/eos/experiment/milliqan/Configs/mqLumis.json', os.getcwd())
+mqLumis = pd.read_json('mqLumis.json', orient = 'split', compression = 'infer')
+
+def checkBeam(mqLumis, filename):
+    run = int(filename.split('Run')[1].split('.')[0])
+    file = int(filename.split('.')[1].split('_')[0])
+    return mqLumis['beam'].loc[(mqLumis['run'] == run) & (mqLumis['file'] == file)].values[0]
+
+#create root TChain of files for a given run
+
+run = 1580
+dataDir = '/store/user/milliqan/trees/v34/1500/'
+
+#create tchain that will read in tree with name t
+mychain = r.TChain('t') 
+
+#add files to mychain
+for filename in os.listdir(dataDir):
+    if not filename.endswith('.root'): continue
+    if 'Run{}'.format(run) in filename:
+        print("Adding file {} to the chain".format(filename))
+        if checkBeam(mqLumis, filename):
+            mychain.Add(dataDir+filename)
+
+#print the number of entries in my tchain
+entries = mychain.GetEntries()
+print("There are {} total entries in the chain".format(entries))
+
+#select only the events with hits in front and back slab with area > 100k
+
+
+mychain.Draw(">>eList", "MaxIf$(area, chan==74) > 100000 && MaxIf$(area, chan==75) > 100000", "entryList")
+eList = r.gDirectory.Get("eList")
+mychain.SetEntryList(eList)
+
+#create a canvas and draw the time difference 
+c1 = r.TCanvas("c1", "c1", 600, 600)
+mychain.Draw("MaxIf$(timeFit_module_calibrated, chan==75&&area==MaxIf$(area,chan==75)) - MaxIf$(timeFit_module_calibrated, chan==74&&area==MaxIf$(area,chan==74)) >> h_timeDiff")
+c1.Draw()
+
+#save the output in a root file
+
+#get all files from the gDirectory
+h_timeDiff = r.gDirectory.Get('h_timeDiff')
+
+#create the output root file
+fout = r.TFile("timingCorrectionsOutput.root", 'RECREATE')
+
+#switch to our root file for writing
+fout.cd()
+
+#write the plot to the file
+h_timeDiff.Write()
+
+#close the file
+fout.Close()

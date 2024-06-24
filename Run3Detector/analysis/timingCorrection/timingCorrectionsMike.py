f06da63f0fbf4320141e9a446e8776986d548cbd
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-06-24 15:13:01 -0400

    Update timingCorrectionsMike.py

--- Run3Detector/analysis/timingCorrection/timingCorrectionsMike.py
+++ Run3Detector/analysis/timingCorrection/timingCorrectionsMike.py
@@ -6,8 +6,6 @@ import pandas as pd
 import json
 import shutil
 
-#%jsroot on
-
 shutil.copy('/eos/experiment/milliqan/Configs/mqLumis.json', os.getcwd())
 mqLumis = pd.read_json('mqLumis.json', orient = 'split', compression = 'infer')
 
@@ -16,33 +14,40 @@ def checkBeam(mqLumis, filename):
     file = int(filename.split('.')[1].split('_')[0])
     return mqLumis['beam'].loc[(mqLumis['run'] == run) & (mqLumis['file'] == file)].values[0]
 
-#create root TChain of files for a given run
-run = 1000######################################################################################################################################
-dataDir = '/store/user/milliqan/trees/v34/1000/'###################################################################################################
+# Define the range of runs
+start_run = 1000
+end_run = 1010
+dataDir = '/store/user/milliqan/trees/v34/'  # Base directory for data
 
-#create tchain that will read in tree with name t
-mychain = r.TChain('t') 
+# Create tchain that will read in tree with name t
+mychain = r.TChain('t')
 
-#add files to mychain
-for filename in os.listdir(dataDir):
-    if not filename.endswith('.root'): continue
-    if 'Run{}'.format(run) in filename:
-        print("Adding file {} to the chain".format(filename))
-        if checkBeam(mqLumis, filename):
-            mychain.Add(dataDir + filename)
+# Loop through the range of runs and add files to the TChain
+for run in range(start_run, end_run + 1):
+    runDir = f'{dataDir}{run}/'
+    if not os.path.exists(runDir):
+        print(f"Directory {runDir} does not exist.")
+        continue
 
-#print the number of entries in my tchain
+    for filename in os.listdir(runDir):
+        if not filename.endswith('.root'): continue
+        if f'Run{run}' in filename:
+            print(f"Adding file {filename} from run {run} to the chain")
+            if checkBeam(mqLumis, filename):
+                mychain.Add(runDir + filename)
+
+# Print the number of entries in my tchain
 entries = mychain.GetEntries()
 print("There are {} total entries in the chain".format(entries))
 
-#select only the events with hits in front and back slab with area > 100k
+# Select only the events with hits in front and back slab with area > 100k
 mychain.Draw(">>eList", "MaxIf$(area, chan==74) > 100000 && MaxIf$(area, chan==75) > 100000", "entryList")
 eList = r.gDirectory.Get("eList")
 print(f"Number of selected entries: {eList.GetN()}")  # Debug print to check selected entries
 mychain.SetEntryList(eList)
 
 if eList.GetN() > 0:
-    #create a canvas and draw the time difference 
+    # Create a canvas and draw the time difference 
     c1 = r.TCanvas("c1", "c1", 600, 600)
     mychain.Draw("MaxIf$(timeFit_module_calibrated, chan==75&&area==MaxIf$(area,chan==75)) - MaxIf$(timeFit_module_calibrated, chan==74&&area==MaxIf$(area,chan==74)) >> h_timeDiff")
     h_timeDiff = r.gDirectory.Get('h_timeDiff')
@@ -51,17 +56,17 @@ if eList.GetN() > 0:
         print(f"Number of entries in histogram: {h_timeDiff.GetEntries()}")  # Debug print to check histogram entries
         if h_timeDiff.GetEntries() > 0:
             c1.Draw()
-            #save the output in a root file
-            #create the output root file
-            fout = r.TFile("Run1000timingCorrection.root", 'RECREATE')##########################################################################################
+            # Save the output in a root file
+            # Create the output root file
+            fout = r.TFile(f"Run{start_run}to{end_run}timingCorrection.root", 'RECREATE')
             
-            #switch to our root file for writing
+            # Switch to our root file for writing
             fout.cd()
 
-            #write the plot to the file
+            # Write the plot to the file
             h_timeDiff.Write()
 
-            #close the file
+            # Close the file
             fout.Close()
         else:
             print("The histogram has no entries.")

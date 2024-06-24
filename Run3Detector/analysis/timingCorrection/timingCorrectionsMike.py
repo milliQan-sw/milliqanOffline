dc0e545de90f45b21745b4d6f08a54578b3a46f5
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-06-24 15:05:05 -0400

    Update timingCorrectionsMike.py

--- Run3Detector/analysis/timingCorrection/timingCorrectionsMike.py
+++ Run3Detector/analysis/timingCorrection/timingCorrectionsMike.py
@@ -5,6 +5,7 @@ import ROOT as r
 import pandas as pd
 import json
 import shutil
+
 #%jsroot on
 
 shutil.copy('/eos/experiment/milliqan/Configs/mqLumis.json', os.getcwd())
@@ -28,7 +29,7 @@ for filename in os.listdir(dataDir):
     if 'Run{}'.format(run) in filename:
         print("Adding file {} to the chain".format(filename))
         if checkBeam(mqLumis, filename):
-            mychain.Add(dataDir+filename)
+            mychain.Add(dataDir + filename)
 
 #print the number of entries in my tchain
 entries = mychain.GetEntries()
@@ -37,25 +38,34 @@ print("There are {} total entries in the chain".format(entries))
 #select only the events with hits in front and back slab with area > 100k
 mychain.Draw(">>eList", "MaxIf$(area, chan==74) > 100000 && MaxIf$(area, chan==75) > 100000", "entryList")
 eList = r.gDirectory.Get("eList")
+print(f"Number of selected entries: {eList.GetN()}")  # Debug print to check selected entries
 mychain.SetEntryList(eList)
 
-#create a canvas and draw the time difference 
-c1 = r.TCanvas("c1", "c1", 600, 600)
-mychain.Draw("MaxIf$(timeFit_module_calibrated, chan==75&&area==MaxIf$(area,chan==75)) - MaxIf$(timeFit_module_calibrated, chan==74&&area==MaxIf$(area,chan==74)) >> h_timeDiff")
-c1.Draw()
+if eList.GetN() > 0:
+    #create a canvas and draw the time difference 
+    c1 = r.TCanvas("c1", "c1", 600, 600)
+    mychain.Draw("MaxIf$(timeFit_module_calibrated, chan==75&&area==MaxIf$(area,chan==75)) - MaxIf$(timeFit_module_calibrated, chan==74&&area==MaxIf$(area,chan==74)) >> h_timeDiff")
+    h_timeDiff = r.gDirectory.Get('h_timeDiff')
 
-#save the output in a root file
-#get all files from the gDirectory
-h_timeDiff = r.gDirectory.Get('h_timeDiff')
+    if h_timeDiff:
+        print(f"Number of entries in histogram: {h_timeDiff.GetEntries()}")  # Debug print to check histogram entries
+        if h_timeDiff.GetEntries() > 0:
+            c1.Draw()
+            #save the output in a root file
+            #create the output root file
+            fout = r.TFile("Run1000timingCorrection.root", 'RECREATE')##########################################################################################
+            
+            #switch to our root file for writing
+            fout.cd()
 
-#create the output root file
-fout = r.TFile("Run1000timingCorrection.root", 'RECREATE')##########################################################################################
+            #write the plot to the file
+            h_timeDiff.Write()
 
-#switch to our root file for writing
-fout.cd()
-
-#write the plot to the file
-h_timeDiff.Write()
-
-#close the file
-fout.Close()
+            #close the file
+            fout.Close()
+        else:
+            print("The histogram has no entries.")
+    else:
+        print("The histogram 'h_timeDiff' was not created.")
+else:
+    print("No entries matched the selection criteria.")

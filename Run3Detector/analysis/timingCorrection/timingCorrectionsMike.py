4e7b8ab6918eefa89f3d72d20c21239a87aaeb87
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-06-25 10:10:36 -0400

    Update timingCorrectionsMike.py

--- Run3Detector/analysis/timingCorrection/timingCorrectionsMike.py
+++ Run3Detector/analysis/timingCorrection/timingCorrectionsMike.py
@@ -20,8 +20,8 @@ def checkBeam(mqLumis, filename):
         return False
 
 # Define the range of runs
-start_run = 1270
-end_run = 1279
+start_run = 1280
+end_run = 1289
 dataDir = '/store/user/milliqan/trees/v34/1200/'  # Base directory for data
 
 # Create TChain that will read in tree with name t

6dcf6068689e33b36227413ad9a85a6520a492b0
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-06-26 14:51:53 -0400

    update

--- Run3Detector/analysis/timingCorrection/4L_timingCorrection.py
+++ Run3Detector/analysis/timingCorrection/4L_timingCorrection.py
@@ -1,121 +1,167 @@
-import sys
-sys.path.append('/root/lib/')
 import os
 import ROOT as r
+import uproot
+import hist
+import matplotlib.pyplot as plt
+import awkward as ak
+import numpy as np
 import pandas as pd
-import json
-import shutil
+import array as arr
+import sys
+sys.path.append('../utilities')
+from milliqanProcessor import *
+from milliqanScheduler import *
+from milliqanCuts import *
+from milliqanPlotter import *
 
-# Copy and load the JSON configuration file
-shutil.copy('/eos/experiment/milliqan/Configs/mqLumis.json', os.getcwd())
-mqLumis = pd.read_json('mqLumis.json', orient='split', compression='infer')
+# define the function to get the time differences
+def getTimeDiff(self):
 
-def checkBeam(mqLumis, filename):
-    try:
-        run = int(filename.split('Run')[1].split('.')[0])
-        file = int(filename.split('.')[1].split('_')[0])
-        beam_value = mqLumis['beam'].loc[(mqLumis['run'] == run) & (mqLumis['file'] == file)].values[0]
-        return beam_value
-    except Exception as e:
-        print(f"Error in checkBeam for file {filename}: {e}")
-        return False
+    time_diffsL30 = []
 
-# Define the range of runs (from Run1000-1009 to Run1620-1629: 62 histograms) 
-start_run = 1620 ####################################################################################################################################
-end_run = 1629 ######################################################################################################################################
-dataDir = '/store/user/milliqan/trees/v34/1600/' ####################################################################################################
+    # height and area mask
+    areaMask = self.events['area'] > 100000
 
-# Create TChain that will read in tree with name t
-mychain = r.TChain('t')
+    # require ipulse == 0
+    finalPulseMask = areaMask & (self.events['ipulse'] == 0)
 
-# Loop through the range of runs and add files to the TChain
-for run in range(start_run, end_run + 1):
-    for filename in os.listdir(dataDir):
-        if not filename.endswith('.root'): continue
-        if f'Run{run}' in filename:
-            print(f"Adding file {filename} from run {run} to the chain")
-            if checkBeam(mqLumis, filename):
-                mychain.Add(dataDir + filename)
-            else:
-                print(f"Beam check failed for file {filename}")
+    # apply the finalPulseMask
+    masked_time = self.events['timeFit_module_calibrated'][finalPulseMask]
+    masked_layer = self.events['layer'][finalPulseMask]
 
-# Print the number of entries in the TChain
-entries = mychain.GetEntries()
-print(f"There are {entries} total entries in the chain")
+    # masked times per layer
+    timeL0 = masked_time[masked_layer == 0]
+    timeL1 = masked_time[masked_layer == 1]
+    timeL2 = masked_time[masked_layer == 2]
+    timeL3 = masked_time[masked_layer == 3]
 
-# Create the output root file
-fout = r.TFile(f"Run{start_run}to{end_run}timingCorrection.root", 'RECREATE')
-fout.cd()
+    # function to get minimum time per event handling None values
+    def minTime(pulse_times):
+        filtered_times = [time for time in pulse_times if time is not None]
+        return min(filtered_times) if filtered_times else None
 
-# Create an empty histogram
-h_timeDiff = r.TH1F("h_timeDiff", f"Run {start_run} to {end_run} time difference", 100, -50, 50)
+    # extract minimum times for each layer
+    timeL0_min = [minTime(event) for event in ak.to_list(timeL0)]
+    timeL1_min = [minTime(event) for event in ak.to_list(timeL1)]
+    timeL2_min = [minTime(event) for event in ak.to_list(timeL2)]
+    timeL3_min = [minTime(event) for event in ak.to_list(timeL3)]
 
-# Ensure there are entries before proceeding
-if entries > 0:
-    # Select only the events with hits in front and back slab with area > 100k
-    mychain.Draw(">>eList", "MaxIf$(area, chan==74) > 100000 && MaxIf$(area, chan==75) > 100000", "entryList")
-    eList = r.gDirectory.Get("eList")
+    for i in range(len(timeL0_min)):
+        # require pulses in all 4 layers for one event
+        if timeL0_min[i] is not None and timeL1_min[i] is not None and timeL2_min[i] is not None and timeL3_min[i] is not None:
+            # calculate time differences only for events with valid times in all layers
+            time_diffsL30.append(timeL3_min[i] - timeL0_min[i])
+    
+    print(time_diffsL30)
 
-    # Check if eList is valid
-    if eList and isinstance(eList, r.TEntryList):
-        print(f"Number of selected entries: {eList.GetN()}")  # Debug print to check selected entries
-        mychain.SetEntryList(eList)
+    # extend the final list to match the size of the current file
+    num_events = len(self.events)
+    num_nones = num_events - len(time_diffsL30)
+    time_diffsL30.extend([None] * num_nones)
 
-        if eList.GetN() > 0:
-            # Create a canvas and draw the time difference 
-            c1 = r.TCanvas("c1", "c1", 600, 600)
-            draw_command = "MaxIf$(timeFit_module_calibrated, chan==75 && area==MaxIf$(area,chan==75)) - MaxIf$(timeFit_module_calibrated, chan==74 && area==MaxIf$(area,chan==74)) >> h_timeDiff"
-            mychain.Draw(draw_command)
-            h_timeDiff = r.gDirectory.Get('h_timeDiff')
+    # define custom branch
+    self.events['timeDiff'] = time_diffsL30
 
-            if h_timeDiff and isinstance(h_timeDiff, r.TH1):
-                print(f"Number of entries in histogram: {h_timeDiff.GetEntries()}")  # Debug print to check histogram entries
-                if h_timeDiff.GetEntries() > 0:
-                    c1.Draw()
-                    # Write the histogram to the file
-                    fout.cd()
-                    h_timeDiff.Write()
-                else:
-                    print("The histogram has no entries.")
-            else:
-                print("The histogram 'h_timeDiff' was not created or is not a TH1 object.")
+# add our custom function to milliqanCuts
+setattr(milliqanCuts, 'getTimeDiff', getTimeDiff)
+
+# check if command line arguments are provided
+if len(sys.argv) != 3:
+    print("Usage: python3 [file_name] [start_run_number] [end_run_number]")
+    sys.exit(1)
+
+# assign start and end indices from command line
+start_run_number = int(sys.argv[1])
+end_run_number = int(sys.argv[2])
+
+# define a file list to run over
+filelist = []
+for run_number in range(start_run_number, end_run_number + 1):
+    file_number = 0
+    consecutive_missing_files = 0
+    while True:
+        file_path = f"/home/bpeng/muonAnalysis/1300/MilliQan_Run{run_number}.{file_number}_v34.root"######################################################################################################
+        if os.path.exists(file_path):
+            filelist.append(file_path)
+            file_number += 1
+            consecutive_missing_files = 0
         else:
-            print("No entries matched the selection criteria.")
-    else:
-        print("Failed to create entry list or eList is not a TEntryList object.")
-else:
-    print("No entries in the TChain.")
+            consecutive_missing_files += 1
+            if consecutive_missing_files >= 10:
+                break
+            file_number += 1
 
-# Close the file
-fout.Close()
+# define the necessary branches to run over
+branches = ['pickupFlag', 'boardsMatched', 'timeFit_module_calibrated', 'height', 'area', 'column', 'row', 'layer', 'chan', 'ipulse', 'type']
 
-# Fit the histogram with a combined model of two Gaussian functions and save the canvas to the ROOT file
+# define the milliqan cuts object
+mycuts = milliqanCuts()
+
+# require pulses are not pickup
+pickupCut = mycuts.getCut(mycuts.pickupCut, 'pickupCut', cut=True, branches=branches)
+
+# require that all digitizer boards are matched
+boardMatchCut = mycuts.getCut(mycuts.boardsMatched, 'boardMatchCut', cut=True, branches=branches)
+
+# add four layer cut
+fourLayerCut = mycuts.getCut(mycuts.fourLayerCut, 'fourLayerCut', cut=False)
+
+# define milliqan plotter
+myplotter = milliqanPlotter()
+
+# create a 1D root histogram
+h_1d = r.TH1F("h_1d", "Time Differences between Layer 3 and 0", 100, -50, 50)
+h_1d.GetXaxis().SetTitle("Time Differences")
+
+# add root histogram to plotter
+myplotter.addHistograms(h_1d, 'timeDiff')
+
+# defining the cutflow
+cutflow = [boardMatchCut, pickupCut, mycuts.layerCut, mycuts.getTimeDiff, myplotter.dict['h_1d']]
+
+# create a schedule of the cuts
+myschedule = milliQanScheduler(cutflow, mycuts, myplotter)
+
+# print out the schedule
+myschedule.printSchedule()
+
+# create the milliqan processor object
+myiterator = milliqanProcessor(filelist, branches, myschedule, mycuts, myplotter)
+
+# run the milliqan processor
+myiterator.run()
+
+# create a new TFile
+f = r.TFile("Run13_0_13_9DtL30.root", "recreate")######################################################################################################
+
+# write the histograms to the file
+h_1d.Write()
+
+# close the file
+f.Close()
+
+# fit the histogram with a combined model of two Gaussian functions and save the canvas to the ROOT file
 def fit_histogram(hist, root_file):
-    if not isinstance(hist, r.TH1):
-        print("Error: The provided object is not a histogram.")
-        return None, None
-
-    # Define the combined Gaussian model
+    # define the combined Gaussian model
     combined_gaus = r.TF1("combined_gaus", "gaus(0) + gaus(3)", -50, 50)
     
-    # Initial parameter estimates for the two Gaussian functions
-    combined_gaus.SetParameters(6, -28.5, 5.145, 115, -5.5, 4.165)  # Max Mean Stddev ######################################################################################################
+    # initial parameter estimates for the two Gaussian functions
+    combined_gaus.SetParameters(4, -17, 3, 26, 0, 4)  # peak mean stddev######################################################################################################
 
-    # Fit the histogram with the combined model
+    # fit the histogram with the combined model
     hist.Fit(combined_gaus, "R")
 
-    # Extract the individual Gaussian functions from the combined model
-    gaus1 = r.TF1("gaus1", "gaus", -40, -18)  # Range ##################################################################################################################################
-    gaus2 = r.TF1("gaus2", "gaus", -12, 3)  # Range ####################################################################################################################################
+    # extract the individual Gaussian functions from the combined model
+    gaus1 = r.TF1("gaus1", "gaus", -24, -10)  # range######################################################################################################
+    gaus2 = r.TF1("gaus2", "gaus", -8, 9)  # range######################################################################################################
     for i in range(3):
         gaus1.SetParameter(i, combined_gaus.GetParameter(i))
         gaus2.SetParameter(i, combined_gaus.GetParameter(i + 3))
 
-    # Get the mean and stddev of the right peak (gaus2)
+    # get the mean of the right peak (gaus2)
     mean_right_peak = gaus2.GetParameter(1)
-    stddev_right_peak = abs(gaus2.GetParameter(2))
 
-    # Draw the histogram and individual fits
+    # draw the histogram and individual fits
     c = r.TCanvas()
     hist.Draw()
     gaus1.SetLineColor(r.kRed)
@@ -123,31 +169,29 @@ def fit_histogram(hist, root_file):
     gaus2.SetLineColor(r.kBlue)
     gaus2.Draw("same")
 
-    # Add the mean value as text on the plot
+    # add the mean value as text on the plot
     text = r.TText()
     text.SetNDC()
     text.SetTextSize(0.03)
     text.DrawText(0.15, 0.85, f"Mean of the right peak: {mean_right_peak:.2f}")
-    text.DrawText(0.15, 0.80, f"Stddev of the right peak: {stddev_right_peak:.2f}")
 
-    # Save the canvas to the ROOT file
+    # save the canvas to the ROOT file
     root_file.cd()
     c.Write("TimeDiffs_Fit_Canvas")
 
-    return mean_right_peak, stddev_right_peak
+    return mean_right_peak
 
-# Create a new TFile for the fitted histogram and canvas
-f_fit = r.TFile(f"FitRun{start_run}to{end_run}timingCorrection.root", "recreate")
+# create a new TFile for the fitted histogram and canvas
+f_fit = r.TFile("Run11_0_11_9DtL30Fit.root", "recreate")######################################################################################################
 
-# Open the original ROOT file and retrieve the histogram
-f_orig = r.TFile(f"Run{start_run}to{end_run}timingCorrection.root")
-h_timeDiff = f_orig.Get("h_timeDiff")
+# open the original ROOT file and retrieve the histogram
+f_orig = r.TFile("Run11_0_11_9DtL30.root")######################################################################################################
+h_1d = f_orig.Get("h_1d")
 
-# Fit the histogram and get the mean of the right peak
-mean_right_peak, stddev_right_peak = fit_histogram(h_timeDiff, f_fit)
+# fit the histogram and get the mean of the right peak
+mean_right_peak = fit_histogram(h_1d, f_fit)
 print("Mean of the right peak:", mean_right_peak)
-print("Stddev of the right peak:", stddev_right_peak)
 
-# Close the files
+# close the files
 f_fit.Close()
 f_orig.Close()

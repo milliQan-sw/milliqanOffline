dc9c9ef2eaf1f449e632124791cd0526be659977
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-06-26 15:07:41 -0400

    update

--- Run3Detector/analysis/timingCorrection/Slab_timingCorrection.py
+++ Run3Detector/analysis/timingCorrection/Slab_timingCorrection.py
@@ -99,7 +99,7 @@ def fit_histogram(hist, root_file):
     combined_gaus = r.TF1("combined_gaus", "gaus(0) + gaus(3)", -50, 50)
     
     # Initial parameter estimates for the two Gaussian functions
-    combined_gaus.SetParameters(6, -28.5, 5.145, 115, -5.5, 4.165)  # Max Mean Stddev ######################################################################################################
+    combined_gaus.SetParameters(6, -28.5, 5.145, 115, -5.5, 4.165)  # Max Mean Stddev ##################################################################################################
 
     # Fit the histogram with the combined model
     hist.Fit(combined_gaus, "R")

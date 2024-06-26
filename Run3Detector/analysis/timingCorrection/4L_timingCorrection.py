a9f3557551b2fb765182bfae590f496611eefa40
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-06-26 15:37:08 -0400

    Update 4L_timingCorrection.py

--- Run3Detector/analysis/timingCorrection/4L_timingCorrection.py
+++ Run3Detector/analysis/timingCorrection/4L_timingCorrection.py
@@ -16,62 +16,62 @@ from milliqanScheduler import *
 from milliqanCuts import *
 from milliqanPlotter import *
 
-# define the function to get the time differences
+# Define the function to get the time differences
 def getTimeDiff(self):
 
     time_diffsL30 = []
 
-    # height and area mask
+    # Height and area mask
     areaMask = self.events['area'] > 100000
 
-    # require ipulse == 0
+    # Pick the first pulse
     finalPulseMask = areaMask & (self.events['ipulse'] == 0)
 
-    # apply the finalPulseMask
+    # Apply the finalPulseMask
     masked_time = self.events['timeFit_module_calibrated'][finalPulseMask]
     masked_layer = self.events['layer'][finalPulseMask]
 
-    # masked times per layer
+    # Masked times per layer
     timeL0 = masked_time[masked_layer == 0]
     timeL1 = masked_time[masked_layer == 1]
     timeL2 = masked_time[masked_layer == 2]
     timeL3 = masked_time[masked_layer == 3]
 
-    # function to get minimum time per event handling None values
+    # Function to get minimum time per event handling None values
     def minTime(pulse_times):
         filtered_times = [time for time in pulse_times if time is not None]
         return min(filtered_times) if filtered_times else None
 
-    # extract minimum times for each layer
+    # Extract minimum times for each layer
     timeL0_min = [minTime(event) for event in ak.to_list(timeL0)]
     timeL1_min = [minTime(event) for event in ak.to_list(timeL1)]
     timeL2_min = [minTime(event) for event in ak.to_list(timeL2)]
     timeL3_min = [minTime(event) for event in ak.to_list(timeL3)]
 
     for i in range(len(timeL0_min)):
-        # require pulses in all 4 layers for one event
+        # Require pulses in all 4 layers for one event
         if timeL0_min[i] is not None and timeL1_min[i] is not None and timeL2_min[i] is not None and timeL3_min[i] is not None:
-            # calculate time differences only for events with valid times in all layers
+            # Calculate time differences only for events with valid times in all layers
             time_diffsL30.append(timeL3_min[i] - timeL0_min[i])
     
     print(time_diffsL30)
 
-    # extend the final list to match the size of the current file
+    # Extend the final list to match the size of the current file
     num_events = len(self.events)
     num_nones = num_events - len(time_diffsL30)
     time_diffsL30.extend([None] * num_nones)
 
-    # define custom branch
+    # Define custom branch
     self.events['timeDiff'] = time_diffsL30
 
-# add our custom function to milliqanCuts
+# Add our custom function to milliqanCuts
 setattr(milliqanCuts, 'getTimeDiff', getTimeDiff)
 
-# define start and end run numbers
+# Define the range of runs (from Run1000-1009 to Run1620-1629: 62 histograms) 
 start_run_number = 1000 ######################################################################################################################################################
 end_run_number = 1009 ########################################################################################################################################################
 
-# define a file list to run over
+# Define a file list to run over
 filelist = []
 for run_number in range(start_run_number, end_run_number + 1):
     file_number = 0
@@ -88,52 +88,52 @@ for run_number in range(start_run_number, end_run_number + 1):
                 break
             file_number += 1
 
-# define the necessary branches to run over
+# Define the necessary branches to run over
 branches = ['pickupFlag', 'boardsMatched', 'timeFit_module_calibrated', 'height', 'area', 'column', 'row', 'layer', 'chan', 'ipulse', 'type']
 
-# define the milliqan cuts object
+# Define the milliqan cuts object
 mycuts = milliqanCuts()
 
-# require pulses are not pickup
+# Require pulses are not pickup
 pickupCut = mycuts.getCut(mycuts.pickupCut, 'pickupCut', cut=True, branches=branches)
 
-# require that all digitizer boards are matched
+# Require that all digitizer boards are matched
 boardMatchCut = mycuts.getCut(mycuts.boardsMatched, 'boardMatchCut', cut=True, branches=branches)
 
-# add four layer cut
+# Add four layer cut
 fourLayerCut = mycuts.getCut(mycuts.fourLayerCut, 'fourLayerCut', cut=False)
 
-# define milliqan plotter
+# Define milliqan plotter
 myplotter = milliqanPlotter()
 
-# create a 1D root histogram
+# Create a 1D root histogram
 h_1d = r.TH1F("h_1d", f"Run {start_run_number} to {end_run_number} time difference", 100, -50, 50)
 
-# add root histogram to plotter
+# Add root histogram to plotter
 myplotter.addHistograms(h_1d, 'timeDiff')
 
-# defining the cutflow
+# Defining the cutflow
 cutflow = [boardMatchCut, pickupCut, mycuts.layerCut, mycuts.getTimeDiff, myplotter.dict['h_1d']]
 
-# create a schedule of the cuts
+# Create a schedule of the cuts
 myschedule = milliQanScheduler(cutflow, mycuts, myplotter)
 
-# print out the schedule
+# Print out the schedule
 myschedule.printSchedule()
 
-# create the milliqan processor object
+# Create the milliqan processor object
 myiterator = milliqanProcessor(filelist, branches, myschedule, mycuts, myplotter)
 
-# run the milliqan processor
+# Run the milliqan processor
 myiterator.run()
 
-# create a new TFile
+# Create a new TFile
 f = r.TFile(f"Run{start_run_number}to{end_run_number}timingCorrection.root", "recreate")
 
-# write the histograms to the file
+# Write the histograms to the file
 h_1d.Write()
 
-# close the file
+# Close the file
 f.Close()
 '''
 # Fit the histogram with a combined model of two Gaussian functions and save the canvas to the ROOT file

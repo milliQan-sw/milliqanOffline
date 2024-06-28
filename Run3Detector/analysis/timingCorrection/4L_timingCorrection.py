bdcb35bd88c99763cb6313c7afb0318aaaace861
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-06-28 10:19:46 -0400

    Update 4L_timingCorrection.py

--- Run3Detector/analysis/timingCorrection/4L_timingCorrection.py
+++ Run3Detector/analysis/timingCorrection/4L_timingCorrection.py
@@ -146,14 +146,14 @@ def fit_histogram(hist, root_file):
     combined_gaus = r.TF1("combined_gaus", "gaus(0) + gaus(3)", -50, 50)
     
     # Initial parameter estimates for the two Gaussian functions
-    combined_gaus.SetParameters(6, -28.5, 5.145, 115, -5.5, 4.165)  # Max Mean Stddev ##################################################################################################
+    combined_gaus.SetParameters(25, -11.5, 4.442, 33, 0, 6.86)  # Max Mean Stddev ##################################################################################################
 
     # Fit the histogram with the combined model
     hist.Fit(combined_gaus, "R")
 
     # Extract the individual Gaussian functions from the combined model
-    gaus1 = r.TF1("gaus1", "gaus", -40, -18)  # Range ##################################################################################################################################
-    gaus2 = r.TF1("gaus2", "gaus", -12, 3)  # Range ####################################################################################################################################
+    gaus1 = r.TF1("gaus1", "gaus", -30, 7)  # Range ##################################################################################################################################
+    gaus2 = r.TF1("gaus2", "gaus", -14, 14)  # Range ####################################################################################################################################
     for i in range(3):
         gaus1.SetParameter(i, combined_gaus.GetParameter(i))
         gaus2.SetParameter(i, combined_gaus.GetParameter(i + 3))

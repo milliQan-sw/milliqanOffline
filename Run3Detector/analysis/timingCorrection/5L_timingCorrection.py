c27df5e36b05294753f7bcd42151b4b2531d60fb
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-06-30 19:34:44 -0400

    Update 5L_timingCorrection.py

--- Run3Detector/analysis/timingCorrection/5L_timingCorrection.py
+++ Run3Detector/analysis/timingCorrection/5L_timingCorrection.py
@@ -86,7 +86,7 @@ for run_number in range(start_run_number, end_run_number + 1):
         if os.path.exists(file_path):
             filelist.append(file_path)
             with uproot.open(file_path) as file:
-                beamOn = file.events['beamOn']
+                beamOn = file['beamOn']
                 if beamOn:
                     beamOn_true_count += 1
                 total_files_count += 1

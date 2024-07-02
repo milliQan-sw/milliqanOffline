1ad61f6e120b725ce1d94aee78fd6fd80102ce2d
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-07-02 15:38:42 -0400

    Update 4LayerIdDt.py

--- Run3Detector/analysis/simAnalysis/4LayerIdDt.py
+++ Run3Detector/analysis/simAnalysis/4LayerIdDt.py
@@ -102,7 +102,7 @@ myiterator = milliqanProcessor(filelist, branches, myschedule, mycuts, myplotter
 myiterator.run()
 
 # create a new TFile
-f = r.TFile("4LayerDtL30.root", "recreate")
+f = r.TFile("TP_Dt.root", "recreate")
 
 # write the histograms to the file
 h_1d.Write()

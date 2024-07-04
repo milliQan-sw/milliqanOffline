61b6bf0755223f151148168adb578111c0fd45f3
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-07-03 21:03:31 -0400

    Update count4LMuon.py

--- Run3Detector/analysis/simAnalysis/count4LMuon.py
+++ Run3Detector/analysis/simAnalysis/count4LMuon.py
@@ -18,18 +18,20 @@ from milliqanPlotter import *
 # define the function to get the number of events with muons in all 4 layers
 def getMuonNum(self):
 
-    countMuon = []
-
-    muons_L0 = self.events['hit_particleName'][abs((self.events['hit_particleName']) == 13) & (self.events['hit_layer'] == 0)]
-    muons_L1 = self.events['hit_particleName'][abs((self.events['hit_particleName']) == 13) & (self.events['hit_layer'] == 1)]
-    muons_L2 = self.events['hit_particleName'][abs((self.events['hit_particleName']) == 13) & (self.events['hit_layer'] == 2)]
-    muons_L3 = self.events['hit_particleName'][abs((self.events['hit_particleName']) == 13) & (self.events['hit_layer'] == 3)]
-
-    for i in range(len(muons_L0)):
-        if ak.any(muons_L0[i]) is not None and ak.any(muons_L1[i]) is not None and ak.any(muons_L2[i]) is not None and ak.any(muons_L3[i]) is not None:
-            countMuon.append(0)
-
-    print(len(countMuon))
+    countMuon = 0
+    
+    # extract muons for each layer
+    muons_L0 = self.events['hit_particleName'][(abs(self.events['hit_particleName']) == 13) & (self.events['hit_layer'] == 0)]
+    muons_L1 = self.events['hit_particleName'][(abs(self.events['hit_particleName']) == 13) & (self.events['hit_layer'] == 1)]
+    muons_L2 = self.events['hit_particleName'][(abs(self.events['hit_particleName']) == 13) & (self.events['hit_layer'] == 2)]
+    muons_L3 = self.events['hit_particleName'][(abs(self.events['hit_particleName']) == 13) & (self.events['hit_layer'] == 3)]
+    
+    # check each event
+    for i in range(len(self.events)):
+        if (ak.any(muons_L0[i]) and ak.any(muons_L1[i]) and ak.any(muons_L2[i]) and ak.any(muons_L3[i])):
+            countMuon += 1
+    
+    print(countMuon)
 
 # add our custom function to milliqanCuts
 setattr(milliqanCuts, 'getMuonNum', getMuonNum)

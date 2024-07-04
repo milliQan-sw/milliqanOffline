37d6ebd3e66d6c651de847d9664aef1ae7a7af64
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-07-04 14:55:12 -0400

    Update BeamFit5L_timingCorrection.py

--- Run3Detector/analysis/timingCorrection/BeamFit5L_timingCorrection.py
+++ Run3Detector/analysis/timingCorrection/BeamFit5L_timingCorrection.py
@@ -156,13 +156,13 @@ def fit_histogram(hist, beamOn_true_percentage, root_file):
     combined_gaus.SetParameters(27, -16.5, 3.48, 18, -1.5, 8.575)  # Max Mean Stddev ######################################################################################################################
 
     # Fit the histogram with the combined model within the specified range
-    fit_range_min = -31  # Adjust this as necessary ######################################################################################################################
-    fit_range_max = 16   # Adjust this as necessary ######################################################################################################################
+    fit_range_min = -31  # Range ######################################################################################################################
+    fit_range_max = 16   # Range ######################################################################################################################
     hist.Fit(combined_gaus, "R", "", fit_range_min, fit_range_max)
 
     # Extract the individual Gaussian functions from the combined model
-    gaus1 = r.TF1("gaus1", "gaus", fit_range_min, fit_range_max)  # Range ######################################################################################################################
-    gaus2 = r.TF1("gaus2", "gaus", fit_range_min, fit_range_max)  # Range ######################################################################################################################
+    gaus1 = r.TF1("gaus1", "gaus", fit_range_min, fit_range_max)
+    gaus2 = r.TF1("gaus2", "gaus", fit_range_min, fit_range_max)
     for i in range(3):
         gaus1.SetParameter(i, combined_gaus.GetParameter(i))
         gaus2.SetParameter(i, combined_gaus.GetParameter(i + 3))

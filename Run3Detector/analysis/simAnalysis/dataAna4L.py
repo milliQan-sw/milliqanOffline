d400bf1e9824ca64c875baf63821026afa42ad32
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-07-24 15:59:23 -0400

    Update dataAna4L.py

--- Run3Detector/analysis/simAnalysis/dataAna4L.py
+++ Run3Detector/analysis/simAnalysis/dataAna4L.py
@@ -64,9 +64,10 @@ def getTimeDiff(self):
             # calculate time differences only for events with valid times in all layers
             time_diffsL30.append(timeL3_min[i] - timeL0_min[i])
     
-    #print(time_diffsL30)
-    #print(len(time_diffsL30))
-    
+    print(time_diffsL30)
+    print(len(time_diffsL30))
+
+    '''
     off = ak.any((self.events['layer'] == 4) & (self.events['nPE'] > 10000), axis = 1)
     sim = ak.any((abs(self.events['hit_particleName']) == 13) & (self.events['hit_layer'] == 4) & (self.events['hit_nPE'] > 10000), axis = 1)
 
@@ -82,7 +83,8 @@ def getTimeDiff(self):
             countsim += 1
 
     print(countoff, countsim)
-
+    '''
+    
     # extend the final list to match the size of the current file
     num_events = len(self.events)
     num_nones = num_events - len(time_diffsL30)

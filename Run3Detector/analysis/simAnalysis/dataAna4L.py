e26536d72e89439a19df2ad3b2255f29c9db76ae
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-07-24 14:44:27 -0400

    Update dataAna4L.py

--- Run3Detector/analysis/simAnalysis/dataAna4L.py
+++ Run3Detector/analysis/simAnalysis/dataAna4L.py
@@ -60,7 +60,7 @@ def getTimeDiff(self):
 
     for i in range(len(timeL0_min)):
         # require an event to have pulses in all 5 layers (this is the actual 5-layer cut being tested) 
-        if timeL0_min[i] is not None and timeL1_min[i] is not None and timeL2_min[i] is not None and timeL3_min[i] is not None and boolTimeL4 is not None:
+        if timeL0_min[i] is not None and timeL1_min[i] is not None and timeL2_min[i] is not None and timeL3_min[i] is not None and boolTimeL4[i] is not None:
             # calculate time differences only for events with valid times in all layers
             time_diffsL30.append(timeL3_min[i] - timeL0_min[i])
     

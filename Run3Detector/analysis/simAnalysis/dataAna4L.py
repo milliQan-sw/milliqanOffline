9409c651e4b8b40d2d1f958825e88d235d1caddd
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-07-30 14:10:12 -0400

    Update dataAna4L.py

--- Run3Detector/analysis/simAnalysis/dataAna4L.py
+++ Run3Detector/analysis/simAnalysis/dataAna4L.py
@@ -19,8 +19,7 @@ from milliqanPlotter import *
 def getTimeDiff(self):
     
     time_diffsL30 = []
-    missed_col = []
-    missed_row = []
+    missed_chan = []
 
     # develop simulation check: events actually have muons in 5 layers (simulation check)
     muonL0Mask = ak.any((abs(self.events['hit_particleName']) == 13) & (self.events['hit_layer'] == 0), axis = 1)
@@ -66,30 +65,22 @@ def getTimeDiff(self):
     timeL2_min = [minTime(event) for event in ak.to_list(timeL2)]
     timeL3_min = [minTime(event) for event in ak.to_list(timeL3)]
 
-    col_L0 = self.events['column'][self.events['layer'] == 0]
-    row_L0 = self.events['row'][self.events['layer'] == 0]
+    chan_L0 = self.events['chan'][self.events['layer'] == 0]
 
     for i in range(len(timeL0_min)):
         # this is the actual cut being tested
         if timeL0_min[i] is not None and timeL1_min[i] is not None and timeL2_min[i] is not None and timeL3_min[i] is not None and backPanelBool[i] == False:
             # calculate time differences only for events with valid times in all layers
             #time_diffsL30.append(timeL3_min[i] - timeL0_min[i])
-            missed_col.append(col_L0[i])
-            missed_row.append(row_L0[i])
-    
-    flattened_col = [item for sublist in missed_col for item in sublist]
-    flattened_row = [item for sublist in missed_row for item in sublist]
-
-    for i in range(len(flattened_col)):
-        print(flattened_col[i], flattened_row[i])
+            missed_chan.append(chan_L0[i])
 
     # extend the final list to match the size of the current file
     num_events = len(self.events)
-    num_nones = num_events - len(time_diffsL30)
+    num_nones = num_events - len(missed_chan)
     time_diffsL30.extend([None] * num_nones)
 
     # define custom branch
-    self.events['timeDiff'] = time_diffsL30
+    self.events['timeDiff'] = missed_chan
 
 # add our custom function to milliqanCuts
 setattr(milliqanCuts, 'getTimeDiff', getTimeDiff)
@@ -106,8 +97,8 @@ mycuts = milliqanCuts()
 myplotter = milliqanPlotter()
 
 # create a 1D root histogram
-h_1d = r.TH1F("h_1d", "Time Differences between Layer 3 and 0", 100, -50, 50)
-h_1d.GetXaxis().SetTitle("Time Differences")
+h_1d = r.TH1F("h_1d", "Chan in Layer 0 of 4 Layer Hit Events Missing Back Slab", 15, 0, 15)
+h_1d.GetXaxis().SetTitle("chan")
 
 # add root histogram to plotter
 myplotter.addHistograms(h_1d, 'timeDiff')
@@ -128,7 +119,7 @@ myiterator = milliqanProcessor(filelist, branches, myschedule, mycuts, myplotter
 myiterator.run()
 
 # create a new TFile
-f = r.TFile("TP_DtL30_4L.root", "recreate")
+f = r.TFile("chan_missed.root", "recreate")
 
 # write the histograms to the file
 h_1d.Write()

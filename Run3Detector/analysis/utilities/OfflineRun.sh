49b13c6854abc31b2eeaccd7a53f7e49916f81c1
Collin Zheng <zheng.1947@osu.edu>
2024-08-07 16:56:28 -0400

    data storage location for checking the correlation btw large hit bar counting vs big hit threashold.

--- Run3Detector/analysis/utilities/OfflineRun.sh
+++ Run3Detector/analysis/utilities/OfflineRun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for ((num = 11; num <= 100; num ++))
+for ((num = 1; num <= 65; num ++))
 do
-    python3 OfflineCosMuon.py 1039  $num /home/czheng/SimCosmicFlatTree/offline_Area_NPE
+    python3 OfflineCosMuon.py 1500  $num /home/czheng/SimCosmicFlatTree/offline_Area_NPE/1500Result/B100bar100
 done   
--- Run3Detector/analysis/utilities/sim_Run.sh
+++ Run3Detector/analysis/utilities/sim_Run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for ((num = 1; num <= 300; num ++))
+for ((num = 301; num <= 1300; num ++))
 do
-    python3 SimCosmicMuonTag_V2.py $num /home/czheng/SimCosmicFlatTree/cutEffCheckResult_sim_20NPEcut/CosmicStraight/purestraightCut/cutflow7
+    python3 simCosMuon.py $num /home/czheng/SimCosmicFlatTree/cutEffCheckResult_sim_20NPEcut/CosmicStraight/purestraightCut/cutflow7
 done    

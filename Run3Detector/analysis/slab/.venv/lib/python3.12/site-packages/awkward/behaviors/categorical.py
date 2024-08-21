73de073e08f4a983375a858385ca2a951ec9a2ac
Ryan De Los Santos <ryansantos1174@gmail.com>
2024-08-20 20:46:07 -0400

    Stop tracking python environment directory

--- Run3Detector/analysis/slab/.venv/lib/python3.12/site-packages/awkward/behaviors/categorical.py
+++
@@ -1,7 +0,0 @@
-# BSD 3-Clause License; see https://github.com/scikit-hep/awkward/blob/main/LICENSE
-
-from __future__ import annotations
-
-from awkward.highlevel import Array
-
-CategoricalBehavior = Array

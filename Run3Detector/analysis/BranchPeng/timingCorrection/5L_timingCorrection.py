14535befc7586b37fe6db4ccb57c4918430103c6
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-08-21 11:43:35 -0400

    Update 5L_timingCorrection.py

--- Run3Detector/analysis/BranchPeng/timingCorrection/5L_timingCorrection.py
+++ Run3Detector/analysis/BranchPeng/timingCorrection/5L_timingCorrection.py
@@ -49,7 +49,7 @@ def getTimeDiff(self):
     timeL1 = ak.where(masked_layer1 == 1, masked_time1, -999999)
     timeL2 = ak.where(masked_layer1 == 2, masked_time1, -999999)
     timeL3 = ak.where(masked_layer1 == 3, masked_time1, -999999)
-    
+
     timeL4 = ak.where(masked_layer2 == 4, masked_time2, -999999)
 
     print(ak.num(timeL0))
@@ -68,7 +68,7 @@ def getTimeDiff(self):
             # Calculate time differences only for events with valid times in all layers
             time_diffsL30.append(timeL3_min[i] - timeL0_min[i])
     
-    print(time_diffsL30)
+    #print(time_diffsL30)
 
     # Extend the final list to match the size of the current file
     num_events = len(self.events)

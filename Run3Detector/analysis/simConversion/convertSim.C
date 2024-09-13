e213b7020e675aa63e46b6b2cf003c3381fa9c04
Ryan De Los Santos <ryansantos1174@gmail.com>
2024-09-13 15:12:56 -0400

    Adding convertSim script

---
+++ Run3Detector/analysis/simConversion/convertSim.C
@@ -0,0 +1,11 @@
+#include "../../../../MilliDAQ/src/V1743Event.cc"
+#include "../../../../MilliDAQ/src/GlobalEvent.cc"
+
+
+int convertSim(char *inputFile, char *outputFile) {
+  mdaq::GlobalEvent event();
+
+  
+
+  return 0;
+}

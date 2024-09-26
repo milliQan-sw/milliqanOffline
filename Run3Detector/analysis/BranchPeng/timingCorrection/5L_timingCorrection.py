69b32cdecdfab1d10c279fa13bcf447412eefe94
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-09-26 15:09:07 -0400

    Update 5L_timingCorrection.py

--- Run3Detector/analysis/BranchPeng/timingCorrection/5L_timingCorrection.py
+++ Run3Detector/analysis/BranchPeng/timingCorrection/5L_timingCorrection.py
@@ -42,11 +42,11 @@ def getTimeDiff(self):
     masked_layer = self.events['layer'][barFinalPulseMask]
 
     # Masked times per layer
-    timeLn1 = masked_time[masked_layer1 == -1]
-    timeL0 = masked_time[masked_layer1 == 0]
-    timeL1 = masked_time[masked_layer1 == 1]
-    timeL2 = masked_time[masked_layer1 == 2]
-    timeL3 = masked_time[masked_layer1 == 3]
+    timeLn1 = masked_time[masked_layer == -1]
+    timeL0 = masked_time[masked_layer == 0]
+    timeL1 = masked_time[masked_layer == 1]
+    timeL2 = masked_time[masked_layer == 2]
+    timeL3 = masked_time[masked_layer == 3]
 
     # Find the minimum time per event (This should be repetitive to ipulse == 0)
     timeLn1_min = ak.min(timeLn1, axis=1, mask_identity=True)

b948ee6480685c9c5205d8f41db003c1250f82b2
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-10-06 19:46:21 -0400

    Update TC_Dt.py

--- Run3Detector/analysis/BranchPeng/timingCorrection/TC_Dt.py
+++ Run3Detector/analysis/BranchPeng/timingCorrection/TC_Dt.py
@@ -31,7 +31,7 @@ def getTimeDiff(self):
 
     # Area mask
     barAreaMask = self.events['nPE'] > 100
-    timeWindowMask = self.events['timeFit_module_calibrated'] > 1000 & self.events['timeFit_module_calibrated'] < 1500
+    timeWindowMask = (self.events['timeFit_module_calibrated'] > 1000) & (self.events['timeFit_module_calibrated'] < 1500)
     topSideMask = self.events['area'][self.events['type'] == 2] < 100000
     print(self.events['layer'][self.events['type'] == 0])
 

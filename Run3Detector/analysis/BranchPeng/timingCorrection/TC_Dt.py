84d1cd7f102f25422adaffd17e5568567f8d0ec5
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-10-06 21:06:04 -0400

    Update TC_Dt.py

--- Run3Detector/analysis/BranchPeng/timingCorrection/TC_Dt.py
+++ Run3Detector/analysis/BranchPeng/timingCorrection/TC_Dt.py
@@ -68,8 +68,11 @@ def getTimeDiff(self):
         else:
             # Append None if an event does not have any pulse in all required layers to keep the branch size
             time_diffsL30.append(None)
-        
-        print(time_diffsL30)    
+         
+
+    for eventIndex in range(len(time_diffsL30)):
+            if time_diffsL30[eventIndex] != None:
+                print(time_diffsL30[eventIndex])  
 
     self.events['timeDiff'] = time_diffsL30
 

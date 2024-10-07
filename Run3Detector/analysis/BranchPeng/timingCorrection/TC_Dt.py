fd9532e2ff3f2a502eda2478c61ef6e031d90dd8
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-10-06 20:14:39 -0400

    Update TC_Dt.py

--- Run3Detector/analysis/BranchPeng/timingCorrection/TC_Dt.py
+++ Run3Detector/analysis/BranchPeng/timingCorrection/TC_Dt.py
@@ -34,7 +34,7 @@ def getTimeDiff(self):
     timeWindowMask = (self.events['timeFit_module_calibrated'] > 1000) & (self.events['timeFit_module_calibrated'] < 1500)
     topSideMask = self.events['area'][self.events['type'] == 2] < 100000
     
-    cutEvents = self.events[self.events['chan'] == 68]
+    cutEvents = self.events[self.events['chan'] == 75]
     print(len(cutEvents))
     for i in range(len(cutEvents)):
         if cutEvents[i] is not None:

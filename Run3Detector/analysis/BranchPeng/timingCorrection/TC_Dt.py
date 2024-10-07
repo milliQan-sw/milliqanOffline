0a3ac48592adfdbcb6f6d91f54468529279cfa1c
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-10-06 20:11:44 -0400

    Update TC_Dt.py

--- Run3Detector/analysis/BranchPeng/timingCorrection/TC_Dt.py
+++ Run3Detector/analysis/BranchPeng/timingCorrection/TC_Dt.py
@@ -37,10 +37,10 @@ def getTimeDiff(self):
     cutEvents = self.events[self.events['chan'] == 0]
     print(len(cutEvents))
     for i in range(len(cutEvents)):
-        if len(cutEvents[i]) != None
-        for j in range(len(cutEvents[i])):
-            if cutEvents[i][j] != None:
-                print(cutEvents['type'][i][j])
+        if len(cutEvents[i]) != None:
+            for j in range(len(cutEvents[i])):
+                if cutEvents[i][j] != None:
+                    print(cutEvents['type'][i][j])
 
 
     # Pick the first pulse

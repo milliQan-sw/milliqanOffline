00abb6cd6f0aa6d693a52df7974949bd49d44960
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-10-07 22:01:37 -0400

    Update TC_Dt.py

--- Run3Detector/analysis/BranchPeng/timingCorrection/TC_Dt.py
+++ Run3Detector/analysis/BranchPeng/timingCorrection/TC_Dt.py
@@ -74,8 +74,8 @@ def getTimeDiff(self):
 setattr(milliqanCuts, 'getTimeDiff', getTimeDiff)
 
 # Define the range of runs
-start_run_number = 1000
-end_run_number = 1626
+start_run_number = 1541
+end_run_number = 1541
 
 # Define a file list to run over
 filelist = []

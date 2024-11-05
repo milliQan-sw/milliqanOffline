5f0f916f522005ca7b75699bfc4a5868747519e8
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-11-05 17:37:17 -0500

    Update resultRawHists.py

--- Run3Detector/analysis/BranchPeng/timingCorrection/resultRawHists.py
+++ Run3Detector/analysis/BranchPeng/timingCorrection/resultRawHists.py
@@ -282,7 +282,7 @@ if __name__ == "__main__":
         "/store/user/milliqan/trees/v35/bar/1700/MilliQan_Run1702.67_v35.root:t",
         "/store/user/milliqan/trees/v35/bar/1700/MilliQan_Run1702.68_v35.root:t"
         ]'''
-    '''filelist = [
+    filelist = [
         #'../skim/MilliQan_Run1000_v35_skim_beamOn_tight.root',
         #'../skim/MilliQan_Run1100_v35_skim_beamOn_tight.root',
         #'../skim/MilliQan_Run1200_v35_skim_beamOn_tight.root',
@@ -291,8 +291,8 @@ if __name__ == "__main__":
         '../skim/MilliQan_Run1500_v35_skim_beamOn_tight.root',
         '../skim/MilliQan_Run1600_v35_skim_beamOn_tight.root',
         '../skim/MilliQan_Run1700_v35_skim_beamOn_tight.root'
-    ]'''
-
+    ]
+    '''
     filelist = [
         '../skim/MilliQan_Run1300_v35_skim_beamOff_tight.root',
         '../skim/MilliQan_Run1400_v35_skim_beamOff_tight.root',
@@ -300,7 +300,7 @@ if __name__ == "__main__":
         '../skim/MilliQan_Run1600_v35_skim_beamOff_tight.root',
         '../skim/MilliQan_Run1700_v35_skim_beamOff_tight.root',
     ]
-
+    '''
     print("Running on files {}".format(filelist))
 
     #find the luminosity of files in filelist

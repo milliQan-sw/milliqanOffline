b636bc22e905c5521bd84326caa46cfa5fb1c72c
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-11-25 13:03:51 -0500

    Update TC_hists.py

--- Run3Detector/analysis/timingCorrection/TC_hists.py
+++ Run3Detector/analysis/timingCorrection/TC_hists.py
@@ -472,6 +472,6 @@ if __name__ == "__main__":
     myschedule.cutFlowPlots()
 
     #save plots
-    myplotter.saveHistograms("timingCorrection{}_5L.root".format('_beamOff'))
+    myplotter.saveHistograms("timingCorrection{}.root".format('_beamOff'))
 
     mycuts.getCutflowCounts()

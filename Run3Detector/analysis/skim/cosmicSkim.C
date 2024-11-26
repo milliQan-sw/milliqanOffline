4a801561e0c0f85c82847b5e042355a92366e98c
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-11-26 13:27:15 -0500

    Update cosmicSkim.C

--- Run3Detector/analysis/skim/cosmicSkim.C
+++ Run3Detector/analysis/skim/cosmicSkim.C
@@ -91,7 +91,7 @@ void myLooper::Loop( TString outFile, TString lumi, TString runTime)
     
       //if (panelHit) continue;
       
-      // Output tree if any path has 4 in a line hit
+      // Output tree if any path has 3 in a line hit
       bool straightLineEvent = false;
       for (int i=0; i<16; i++) {
             if (!frontPanelHit && i < 8) continue;

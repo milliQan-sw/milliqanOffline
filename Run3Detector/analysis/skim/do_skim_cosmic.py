ec46788d42f4b328c319fb2f04ee6995e36ddf15
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-11-26 15:22:28 -0500

    Update do_skim_cosmic.py

--- Run3Detector/analysis/skim/do_skim_cosmic.py
+++ Run3Detector/analysis/skim/do_skim_cosmic.py
@@ -27,8 +27,8 @@ goodRuns = pd.read_json('goodRunsList.json', orient = 'split', compression = 'in
 
 ########################################################
 ################### Settings ##########################
-directory = '/store/user/milliqan/trees/v35/bar/1500/'
-outputName = 'MilliQan_Run1500_v35_cosmic_beamOff_tight.root'
+directory = '/store/user/milliqan/trees/v35/bar/1300/'
+outputName = 'MilliQan_Run1300_v35_cosmic_beamOff_tight.root'
 beam = False
 goodRun = 'goodRunTight'
 skimType = 'cosmic'

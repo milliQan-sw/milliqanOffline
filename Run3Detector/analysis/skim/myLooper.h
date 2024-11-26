698d6929d9135da4606efed65709310727a38a15
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2024-11-26 15:05:00 -0500

    Update myLooper.h

--- Run3Detector/analysis/skim/myLooper.h
+++ Run3Detector/analysis/skim/myLooper.h
@@ -171,7 +171,7 @@ public :
    virtual Int_t    GetEntry(Long64_t entry);
    virtual Long64_t LoadTree(Long64_t entry);
    virtual void     Init(TTree *tree);
-   virtual void     Loop(TString outfile);
+   virtual void     Loop(TString outfile, TString lumi, TString runTime);
    virtual Bool_t   Notify();
    virtual void     Show(Long64_t entry = -1);
 };

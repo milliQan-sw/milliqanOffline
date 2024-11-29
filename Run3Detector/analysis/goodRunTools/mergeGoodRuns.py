7781cfc152dec6ba995e9b0c4e3754a0e62b10bf
Michael William Carrigan <michael.william.carrigan@cern.ch>
2024-11-29 21:10:30 +0100

    Updates to Good Runs and Lumi Plotting

--- Run3Detector/analysis/goodRunTools/mergeGoodRuns.py
+++ Run3Detector/analysis/goodRunTools/mergeGoodRuns.py
@@ -14,7 +14,7 @@ from checkMatching import fileChecker
 if __name__ == "__main__":
     
     #input directory will all good run lists
-    dataDir = '/data/users/milliqan/log/goodRunLists/currentVersion/09_06_14/'
+    dataDir = '/data/users/milliqan/log/goodRunLists/currentVersion/11_27_10/'
 
     goodRuns = pd.DataFrame()
     checks = pd.DataFrame()

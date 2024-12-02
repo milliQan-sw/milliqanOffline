65ed1a5dbda99468ea7d908480616cafa9ad4312
ryansantos1174 <ryansantos1174@gmail.com>
2024-12-02 10:05:34 -0500

    Remove tracking of launch_singularity script

--- Run3Detector/analysis/simConversion/utils/launch_singularity.sh
+++
@@ -1,1 +0,0 @@
-singularity shell --bind /abyss/users/rsantos/MilliQan/:/data,/store/user/milliqan/trees/v35/bar/1900/:/store  /cvmfs/unpacked.cern.ch/registry.hub.docker.com/carriganm95/milliqan_offline:x86/

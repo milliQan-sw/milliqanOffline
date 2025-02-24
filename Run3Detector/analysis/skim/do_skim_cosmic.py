1187c5417e7dea9b7379bf02980d617913b575d4
Branch Peng <126245822+Yujie-Peng@users.noreply.github.com>
2025-02-23 20:39:27 -0500

    Update do_skim_cosmic.py

--- Run3Detector/analysis/skim/do_skim_cosmic.py
+++ Run3Detector/analysis/skim/do_skim_cosmic.py
@@ -6,19 +6,6 @@ import sys
 sys.path.append('../utilities')
 from utilities import *
 
-def checkBeam(mqLumis, run, file, branch='beam'):
-    #print("check beam run {} file {}".format(run, file))
-    beam = mqLumis[branch].loc[(mqLumis['run'] == run) & (mqLumis['file'] == file)]
-    if beam.size == 0: return None
-    beam = beam.values[0]
-    return beam
-
-def checkGoodRun(goodRuns, run, file, branch='goodRunTight'):
-    goodRun = goodRuns[branch].loc[(goodRuns['run'] == run) & (goodRuns['file'] == file)].values
-    if len(goodRun) == 1:
-        return goodRun[0]
-    return False
-
 ########################################################
 ################### Settings ##########################
 directory = '/store/user/milliqan/trees/v35/slab/1000/'
@@ -54,18 +41,6 @@ for ifile, filename in enumerate(os.listdir(directory)):
 
     run = int(filename.split('Run')[1].split('.')[0])
     file = int(filename.split('.')[1].split('_')[0])
-
-    #check if there is beam if desired
-    if beam is not None:
-        beamOn = checkBeam(mqLumis, run, file, branch='beamInFill')
-        if beamOn == None: continue
-        if (beam and not beamOn) or (not beam and beamOn): continue
-
-    #check good runs list
-    if goodRun is not None:
-        isGoodRun = checkGoodRun(goodRuns, run, file, branch=goodRun)
-
-        if not isGoodRun: continue
 
     print(filename)
 
